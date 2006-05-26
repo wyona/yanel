d5b4413468500962d166b01ef1f03874259bca02
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-05-26 07:03:05 +0000

    build and configure enhanced

--- build.sh
+++ build.sh
@@ -1,3 +1,39 @@
 #!/bin/sh
 
-echo "Build Yanel ..."
+echo "INFO: Build Yanel ..."
+
+# ----- Parameters
+
+# ----- Check for JAVA_HOME
+JAVA_HOME="$JAVA_HOME"
+if [ "$JAVA_HOME" = "" ];then
+  echo "ERROR: No JAVA_HOME set!"
+  echo "       Have you installed JDK (Java Development Kit)?"
+  exit 1
+fi
+
+# ----- Check Java version
+# TODO: ....
+
+# ----- Set Environment Variables
+ORIGINAL_ANT_HOME=$ANT_HOME
+unset ANT_HOME
+ANT_HOME=$PWD/tools/apache-ant-1.6.2
+#echo $ANT_HOME
+
+ORIGINAL_PATH=$PATH
+PATH=$ANT_HOME/bin:$PATH
+#echo $PATH
+
+# ----- Build Yanel ...
+ant -f src/build/build.xml $@
+
+# ----- Reset Environment Variables
+ANT_HOME=$ORIGINAL_ANT_HOME
+#echo $ANT_HOME
+PATH=$ORIGINAL_PATH
+#echo $PATH
+
+echo "INFO: Build has been completed successfully :-)"
+echo ""
+echo "INFO: Start using Yanel by executing yanel.sh"
