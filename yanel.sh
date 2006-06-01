ae930a7c907d821bfa9b06557f31af1a80071e43
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-06-01 22:21:05 +0000

    log4j properties path added

--- yanel.sh
+++ yanel.sh
@@ -4,4 +4,4 @@ echo "Start/Stop Yanel ..."
 
 MAVEN_REPO_LOCAL=/home/michi/.m2/repository
 
-java -classpath build/classes:src/resources/tape/build/classes:src/resources/invoice/build/classes:src/resources/websearch/build/classes:$MAVEN_REPO_LOCAL/yarep/yarep/0.0.1-dev-r13790/yarep-0.0.1-dev-r13790.jar:$MAVEN_REPO_LOCAL/log4j/log4j/1.2.8/log4j-1.2.8.jar org.wyona.yanel.cmdl.YanelCommandLine
+java -classpath build/classes:lib:src/resources/tape/build/classes:src/resources/invoice/build/classes:src/resources/websearch/build/classes:$MAVEN_REPO_LOCAL/yarep/yarep/0.0.1-dev-r13790/yarep-0.0.1-dev-r13790.jar:$MAVEN_REPO_LOCAL/log4j/log4j/1.2.8/log4j-1.2.8.jar org.wyona.yanel.cmdl.YanelCommandLine
