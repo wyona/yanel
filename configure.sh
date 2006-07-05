4540f763a5aefe88f30394609a89fc027226700c
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-07-05 05:56:48 +0000

    note about ac-policies properties added

--- configure.sh
+++ configure.sh
@@ -37,7 +37,7 @@ echo "NOTE 1: Copy src/build/build.properties to src/build/local.build.propertie
 echo ""
 
 echo ""
-echo "NOTE 2: Copy lib/yanel-rti-yarep.properties to lib/local.yanel-rti-yarep.properties and copy lib/yarep.properties to lib/local.yarep.properties. Customize the local.*.properties files and run build.sh! Or run build.sh first and then customize the *.properties files within build/webapps/yanel/WEB-INF/classes"
+echo "NOTE 2: Copy lib/yanel-rti-yarep.properties to lib/local.yanel-rti-yarep.properties, copy lib/yarep.properties to lib/local.yarep.properties and copy lib/ac-policies-yarep.properties to lib/local.ac-policies-yarep.properties. Customize the local.*.properties files and run build.sh! Or run build.sh first and then customize the *.properties files within build/webapps/yanel/WEB-INF/classes"
 echo ""
 
 # ----- Reset Environment Variables
