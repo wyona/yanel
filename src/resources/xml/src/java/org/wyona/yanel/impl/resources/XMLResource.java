7257620f35aac72570c0c23b8287dab5d6888cfa
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-07-12 21:19:38 +0000

    XML resource started

---
+++ src/resources/xml/src/java/org/wyona/yanel/impl/resources/XMLResource.java
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2006 Wyona
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.wyona.org/licenses/APACHE-LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.wyona.yanel.impl.resources;
+
+import org.wyona.yanel.core.Path;
+import org.wyona.yanel.core.Resource;
+import org.wyona.yanel.core.Topic;
+import org.wyona.yanel.core.api.attributes.ModifiableV1;
+import org.wyona.yanel.core.api.attributes.ViewableV1;
+import org.wyona.yanel.core.attributes.viewable.View;
+import org.wyona.yanel.core.attributes.viewable.ViewDescriptor;
+
+import org.wyona.yarep.core.Repository;
+import org.wyona.yarep.core.RepositoryFactory;
+import org.wyona.yarep.util.RepoPath;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.transform.stream.StreamResult;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+
+import org.apache.log4j.Category;
+
+/**
+ *
+ */
+public class XMLResource extends Resource implements ViewableV1, ModifiableV1 {
+
+    private static Category log = Category.getInstance(XMLResource.class);
+
+    /**
+     *
+     */
+    public XMLResource() {
+    }
+
+    /**
+     *
+     */
+    public ViewDescriptor[] getViewDescriptors() {
+        return null;
+    }
+
+    /**
+     *
+     */
+    public View getView(Path path, String viewId) {
+        View defaultView = new View();
+        String mimeType = getMimeType(path, viewId);
+        defaultView.setMimeType(mimeType);
+
+        try {
+            RepoPath rp = new org.wyona.yarep.util.YarepUtil().getRepositoryPath(new org.wyona.yarep.core.Path(path.toString()), new RepositoryFactory());
+
+            if (mimeType.equals("application/xml")) {
+                defaultView.setInputStream(getContentXML(rp));
+                return defaultView;
+	    } else if (mimeType.equals("application/xhtml+xml")) {
+                File xsltFile = org.wyona.commons.io.FileUtil.file(rtd.getConfigFile().getParentFile().getAbsolutePath(), "document2xhtml.xsl");
+                log.error("DEBUG: XSLT file: " + xsltFile);
+                Transformer transformer = TransformerFactory.newInstance().newTransformer(new StreamSource(xsltFile));
+                 // TODO: Is this the best way to generate an InputStream from an OutputStream?
+                java.io.ByteArrayOutputStream baos = new java.io.ByteArrayOutputStream();
+                transformer.transform(new StreamSource(getContentXML(rp)), new StreamResult(baos));
+                defaultView.setInputStream(new java.io.ByteArrayInputStream(baos.toByteArray()));
+                return defaultView;
+            } else {
+                log.debug("Mime-Type: " + mimeType);
+                defaultView.setInputStream(rp.getRepo().getInputStream(new org.wyona.yarep.core.Path(rp.getPath().toString())));
+            }
+        } catch(Exception e) {
+            log.error(e);
+        }
+
+        return defaultView;
+    }
+
+    /**
+     *
+     */
+    private InputStream getContentXML(RepoPath rp) throws Exception {
+                JarInputStream jarStream = new JarInputStream((rp.getRepo().getInputStream(new org.wyona.yarep.core.Path(rp.getPath().toString()))));
+                JarEntry jarEntry;
+                while ((jarEntry = jarStream.getNextJarEntry()) != null) {
+                    log.debug("Jar Entry Name: " + jarEntry.getName());
+                    if (jarEntry.getName().equals("content.xml")) {
+                        return jarStream;
+                    }
+                // TODO: What if zip does not contain a "content.xml"?!
+                }
+        return null;
+    }
+
+    /**
+     *
+     */
+    private String getMimeType(Path path, String viewId) {
+        String mimeType = null;
+        try {
+            // TODO: Get yanel RTI yarep properties file name from framework resp. use MapFactory ...!
+            RepoPath rpRTI = new org.wyona.yarep.util.YarepUtil().getRepositoryPath(new org.wyona.yarep.core.Path(path.toString()), new RepositoryFactory("yanel-rti-yarep.properties"));
+            java.io.BufferedReader br = new java.io.BufferedReader(rpRTI.getRepo().getReader(new org.wyona.yarep.core.Path(new Path(rpRTI.getPath().toString()).getRTIPath().toString())));
+            br.readLine();
+            mimeType = br.readLine();
+            if (mimeType != null) {
+                if (mimeType.indexOf("mime-type:") == 0) {
+                    mimeType = mimeType.substring(11);
+                    log.info("*" + mimeType + "*");
+                    // TODO: Maybe validate mime-type ...
+                    return mimeType;
+                }
+            }
+        } catch(Exception e) {
+            log.warn(e);
+        }
+
+        String suffix = path.getSuffix();
+        if (suffix != null) {
+            log.debug("SUFFIX: " + suffix);
+            if (suffix.equals("html")) {
+                //mimeType = "text/html";
+                mimeType = "application/xhtml+xml";
+            } else if (suffix.equals("xhtml")) {
+                mimeType = "application/xhtml+xml";
+            } else if (suffix.equals("xml")) {
+                mimeType = "application/xml";
+            } else {
+                mimeType = "application/vnd.oasis.opendocument.text";
+            }
+        } else {
+            mimeType = "application/vnd.oasis.opendocument.text";
+        }
+        return mimeType;
+    }
+
+    /**
+     *
+     */
+    public View getView(HttpServletRequest request, String viewId) {
+        return getView(new Path(request.getServletPath()), viewId);
+    }
+
+    /**
+     *
+     */
+    public Reader getReader(Path path) {
+        try {
+            RepoPath rp = new org.wyona.yarep.util.YarepUtil().getRepositoryPath(new org.wyona.yarep.core.Path(path.toString()), new RepositoryFactory());
+            return rp.getRepo().getReader(new org.wyona.yarep.core.Path(rp.getPath().toString()));
+        } catch(Exception e) {
+            log.error(e);
+        }
+        return null;
+    }
+
+    /**
+     *
+     */
+    public Reader getReader(Topic topic) {
+        log.error("Not implemented yet!");
+        return null;
+    }
+
+    /**
+     *
+     */
+    public Writer getWriter(Path path) {
+        log.error("Not implemented yet!");
+        return null;
+    }
+
+    /**
+     *
+     */
+    public Writer getWriter(Topic topic) {
+        log.error("Not implemented yet!");
+        return null;
+    }
+
+    /**
+     *
+     */
+    public OutputStream getOutputStream(Path path) {
+        try {
+            RepoPath rp = new org.wyona.yarep.util.YarepUtil().getRepositoryPath(new org.wyona.yarep.core.Path(path.toString()), new RepositoryFactory());
+            return rp.getRepo().getOutputStream(new org.wyona.yarep.core.Path(rp.getPath().toString()));
+        } catch(Exception e) {
+            log.error(e);
+        }
+        return null;
+    }
+}
