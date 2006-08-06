56f4d7245b01d6469ccc62bfa9e6ed2f1a7c57eb
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-08-06 22:09:31 +0000

    email import through POP started

---
+++ src/core/java/org/wyona/yanel/cmdl/POP.java
@@ -0,0 +1,12 @@
+package org.wyona.yanel.cmdl;
+
+/**
+ *
+ */
+public class POP {
+
+    String server = "mail.wyona.com";
+    String username = "yanel";
+    String password = "yanel";
+    String path = "/mailing-lists/ulysses/general";
+}
