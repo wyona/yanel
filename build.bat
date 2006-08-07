3134caea7797fe598d3b34729b7b3148a7060b08
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-08-07 22:11:00 +0000

    build bat for windows added

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem ----- Ignore system ANT_HOME variable
+set ORIGINAL_ANT_HOME=%ANT_HOME%
+set ANT_HOME=tools\apache-ant-1.6.2
+
+call %ANT_HOME%\bin\ant -version
+rem call %ANT_HOME%\bin\ant -f src\build\build.xml
+call %ANT_HOME%\bin\ant -f src\build\build.xml -Djava.endorsed.dirs=lib\endorsed -logger org.apache.tools.ant.NoBannerLogger -emacs %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+rem ----- Restore ANT_HOME
+set ANT_HOME=%ORIGINAL_ANT_HOME%
+set ORIGINAL_ANT_HOME=
