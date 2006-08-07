6ea5e4bf6567715073db94bdaf13b9f5e53d29c7
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-08-07 22:37:41 +0000

    yanel bat for Windows added

---
+++ yanel.bat
@@ -0,0 +1,14 @@
+@echo off
+
+rem ----- Ignore system ANT_HOME variable
+set ORIGINAL_ANT_HOME=%ANT_HOME%
+set ANT_HOME=tools\apache-ant-1.6.2
+
+call %ANT_HOME%\bin\ant -version
+rem call %ANT_HOME%\bin\ant -f src\build\build.xml
+call %ANT_HOME%\bin\ant -f src\build\build.xml run-yanel-cmdl
+rem call %ANT_HOME%\bin\ant -f src\build\build.xml -Djava.endorsed.dirs=lib\endorsed -logger org.apache.tools.ant.NoBannerLogger -emacs run-yanel-cmdl
+
+rem ----- Restore ANT_HOME
+set ANT_HOME=%ORIGINAL_ANT_HOME%
+set ORIGINAL_ANT_HOME=
