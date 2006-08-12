5368cd028232b3fd6f79288c0b45fbdf61d8d2cf
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-08-12 23:11:49 +0000

    last modified added

---
+++ src/core/java/org/wyona/yanel/core/api/attributes/ModifiableV2.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2006 Wyona
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.wyona.org/licenses/APACHE-LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.wyona.yanel.core.api.attributes;
+
+import org.wyona.yanel.core.Path;
+import org.wyona.yanel.core.Topic;
+
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+
+/**
+ *
+ */
+public interface ModifiableV2 {
+    // TODO: Shouldn't it be the other way around, that the Resource is passed to a Repository instead the Repository to the Resource?!
+    //TODO: Shouldn't we use java.io.Serializable (writeObject, readObject)?!
+
+    /**
+     *
+     */
+    public Reader getReader(Path path) throws Exception;
+
+    /**
+     *
+     */
+    public Reader getReader(Topic topic) throws Exception;
+
+    /**
+     *
+     */
+    public Writer getWriter(Path path);
+
+    /**
+     *
+     */
+    public Writer getWriter(Topic topic);
+
+    /**
+     *
+     */
+    public OutputStream getOutputStream(Path path);
+
+    /**
+     *
+     */
+    public long getLastModified(Path path);
+}
