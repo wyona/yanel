0dabdf1aa321e1ea415cf231cb7130225e08fe7a
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-08-24 14:42:54 +0000

    nutch resource added

---
+++ src/contributions/resources/nutch/src/java/org/wyona/yanel/impl/resources/NutchResource.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2006 Wyona
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.wyona.org/licenses/APACHE-LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.wyona.yanel.impl.resources;
+
+import org.wyona.yanel.core.ResourceTypeDefinition;
+import org.wyona.yanel.core.api.attributes.ViewableV1;
+import org.wyona.yanel.core.attributes.viewable.ViewDescriptor;
+
+import org.wyona.yanel.impl.ResourceDefaultImpl;
+
+/**
+ *
+ */
+public class NutchResource extends ResourceDefaultImpl implements ViewableV1 {
+
+    /**
+     *
+     */
+    public NutchResource() {
+    }
+
+    /**
+     *
+     */
+    public ViewDescriptor[] getViewDescriptors() {
+        return null;
+    }
+}
