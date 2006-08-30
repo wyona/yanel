ee54bbbc92c37d7d2035cae364bae54b64190bd1
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-08-30 12:53:42 +0000

    zip started

---
+++ src/contributions/resources/zip/src/java/org/wyona/yanel/impl/resources/ZipResource.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2006 Wyona
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.wyona.org/licenses/APACHE-LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.wyona.yanel.impl.resources;
+
+import org.wyona.yanel.core.Path;
+import org.wyona.yanel.core.Resource;
+import org.wyona.yanel.core.api.attributes.ViewableV1;
+import org.wyona.yanel.core.attributes.viewable.View;
+import org.wyona.yanel.core.attributes.viewable.ViewDescriptor;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.wyona.yarep.core.NoSuchNodeException;
+import org.wyona.yarep.core.Repository;
+import org.wyona.yarep.core.RepositoryFactory;
+import org.wyona.yarep.util.RepoPath;
+
+import org.apache.log4j.Category;
+
+import java.io.File;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.transform.stream.StreamResult;
+
+/**
+ *
+ */
+public class ZipResource extends Resource implements ViewableV1 {
+
+    private static Category log = Category.getInstance(ZipResource.class);
+
+    /**
+     *
+     */
+    public ZipResource() {
+    }
+
+    /**
+     *
+     */
+    public ViewDescriptor[] getViewDescriptors() {
+        return null;
+    }
+
+    /**
+     *
+     */
+    public View getView(Path path, String viewId) {
+        View defaultView = new View();
+
+        Repository contentRepo = null;
+        try {
+            RepoPath rp = new org.wyona.yarep.util.YarepUtil().getRepositoryPath(new org.wyona.yarep.core.Path(path.toString()), new RepositoryFactory());
+            contentRepo = rp.getRepo();
+
+            String zipDir = getProperty(path, "zip-dir");
+            log.error("DEBUG: Zip Directory: " + zipDir);
+        } catch(Exception e) {
+            log.error(e);
+        }
+
+        defaultView.setMimeType("application/neutron-archive");
+	defaultView.setInputStream(null);
+        return defaultView;
+    }
+
+    /**
+     *
+     */
+    public View getView(HttpServletRequest request, String viewId) {
+        return getView(new Path(request.getServletPath()), viewId);
+    }
+
+    /**
+     *
+     */
+    private String getProperty(Path path, String name) {
+        String property = null;
+        try {
+            // TODO: Get yanel RTI yarep properties file name from framework resp. use MapFactory ...!
+            RepoPath rpRTI = new org.wyona.yarep.util.YarepUtil().getRepositoryPath(new org.wyona.yarep.core.Path(path.toString()), new RepositoryFactory("yanel-rti-yarep.properties"));
+            java.io.BufferedReader br = new java.io.BufferedReader(rpRTI.getRepo().getReader(new org.wyona.yarep.core.Path(new Path(rpRTI.getPath().toString()).getRTIPath().toString())));
+
+            while ((property = br.readLine()) != null) {
+                if (property.indexOf(name + ":") == 0) {
+                    property = property.substring(name.length() + 2);
+                    log.info("*" + property + "*");
+                    return property;
+                }
+            }
+        } catch(Exception e) {
+            log.warn(e);
+        }
+
+        return property;
+    }
+}
