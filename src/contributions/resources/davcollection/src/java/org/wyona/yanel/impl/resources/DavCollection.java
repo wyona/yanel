cd53d9c3c94e4286831f3c1f23ab975a1b0ca7e5
imboden <imboden@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-09-21 14:44:53 +0000

    added a WebDAV collection resource, works atleas with konqueror

---
+++ src/contributions/resources/davcollection/src/java/org/wyona/yanel/impl/resources/DavCollection.java
@@ -0,0 +1,179 @@
+package org.wyona.yanel.impl.resources;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.log4j.Category;
+import org.wyona.yanel.core.Path;
+import org.wyona.yanel.core.api.attributes.ViewableV2;
+import org.wyona.yarep.core.Repository;
+import org.wyona.yarep.core.RepositoryFactory;
+import org.wyona.yarep.util.RepoPath;
+import org.xml.sax.SAXException;
+
+public class DavCollection extends XmlViewResource implements ViewableV2 {
+
+    private static Category log = Category.getInstance(DavCollection.class);
+    
+    // WebDAV namespace definition
+    private static final String WEBDAV_NS_PREFIX = "D";
+    private static final String WEBDAV_NS_URI = "DAV:";
+    
+    // WebDav element definitions
+    private static final String WEBDAV_EL_MSTATUS   = "multistatus";
+    private static final String WEBDAV_EL_STATUS    = "status";
+    private static final String WEBDAV_EL_HREF      = "href";
+    private static final String WEBDAV_EL_RESPONSE  = "response";
+    private static final String WEBDAV_EL_PROPSTAT  = "propstat";
+    private static final String WEBDAV_EL_PROP      = "prop";
+    private static final String WEBDAV_EL_DISPNAME  = "displayname";
+    private static final String WEBDAV_EL_LASTMOD   = "lastmodified";
+    private static final String WEBDAV_EL_CREATDATE = "creationdate";
+    private static final String WEBDAV_EL_RESTYPE   = "resourcetype";
+    private static final String WEBDAV_EL_CONTTYPE  = "contenttype";
+    private static final String WEBDAV_EL_CONTLEN   = "contentlength";
+    private static final String WEBDAV_EL_LOCKDISC  = "lockdiscovery";
+    private static final String WEBDAV_EL_COLL      = "collection";
+    private static final String WEBDAV_EL_RES       = "resource";
+    
+   
+    
+    public DavCollection() throws ParserConfigurationException, IOException {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public void buildXmlView(Path path, String viewId, HttpServletRequest request, HttpServletResponse response) throws SAXException {
+
+        response.setStatus(207, "Multi Status");
+        
+        startDocument();          
+        startPrefixMapping(WEBDAV_NS_PREFIX, WEBDAV_NS_URI);      
+        startElement(WEBDAV_NS_PREFIX, WEBDAV_EL_MSTATUS, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_MSTATUS, null);
+        
+        // copyied from DirectoryResource.java, modified to use a ContentHandler
+        // instead of a StringBuffer for the XML view 
+        
+        Repository contentRepo = null;
+        try {
+            RepoPath rp = new org.wyona.yarep.util.YarepUtil().getRepositoryPath(new org.wyona.yarep.core.Path(path.toString()), new RepositoryFactory());
+            contentRepo = rp.getRepo();
+            org.wyona.yarep.core.Path p = rp.getPath();
+
+            // TODO: This doesn't seem to work ... (check on Yarep ...)
+            if (contentRepo.isResource(p)) {
+                log.warn("Path is a resource instead of a collection: " + p);
+                // p = p.getParent();
+            }
+
+            // TODO: Implement org.wyona.yarep.core.Path.getParent()
+            if (!contentRepo.isCollection(p)) {
+                log.warn("Path is not a collection: " + p);
+                p = new org.wyona.yarep.core.Path(new org.wyona.commons.io.Path(p.toString()).getParent().toString());
+                log.warn("Use parent of path: " + p);
+            }
+         
+            
+            this.propfindAddResource(request.getContextPath() + path.toString(), 
+                    path.getName(), 
+                    WEBDAV_EL_COLL,
+                    "httpd/unix-directory",
+                    new Date(contentRepo.getLastModified(p)).toGMTString());
+            
+            // TODO: Do not show the children with suffix .yanel-rti resp. make
+            // this configurable!
+            // NOTE: Do not hardcode the .yanel-rti, but rather use
+            // Path.getRTIPath ...
+            
+            org.wyona.yarep.core.Path[] children = contentRepo.getChildren(p);
+            for (int i = 0; i < children.length; i++) {
+                if (contentRepo.isResource(children[i])) {
+                    this.propfindAddResource(request.getContextPath() + children[i].toString(), 
+                            children[i].getName(), 
+                            WEBDAV_EL_RES,
+                            "httpd/unix-directory",
+                            new Date(contentRepo.getLastModified(children[i])).toGMTString());
+                } else if (contentRepo.isCollection(children[i])) {
+                    this.propfindAddResource(request.getContextPath() + children[i].toString(), 
+                            children[i].getName(), 
+                            WEBDAV_EL_COLL,
+                            "httpd/unix-directory",
+                            new Date(contentRepo.getLastModified(children[i])).toGMTString());
+                } else {
+                    log.debug("####### neither collection nor resource");
+                }
+            }
+            
+        } catch (Exception e) {
+            log.error(e);
+        }
+        
+        endElement(WEBDAV_NS_URI, WEBDAV_EL_MSTATUS, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_MSTATUS);
+        endPrefixMapping(WEBDAV_NS_PREFIX);
+        endDocument();
+    }
+    
+    /**
+     * Adds a resource to a propfind response
+     * @param href
+     * @param displayName
+     * @param type
+     * @param mimeType
+     * @param lastModified
+     * @throws SAXException
+     */
+    void propfindAddResource(String href, String displayName, String type, String mimeType, String lastModified ) throws SAXException {
+        // response
+        startElement(WEBDAV_NS_URI, WEBDAV_EL_RESPONSE, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_RESPONSE, null);            
+        // href
+        startElement(WEBDAV_NS_URI, WEBDAV_EL_HREF, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_HREF, null);
+        characters(href);
+        endElement(WEBDAV_NS_URI, WEBDAV_EL_HREF, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_HREF);
+        // propstat
+        startElement(WEBDAV_NS_URI, WEBDAV_EL_PROPSTAT, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_PROPSTAT, null);
+        //properties            
+        startElement(WEBDAV_NS_URI, WEBDAV_EL_PROP, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_PROP, null);
+        // displayname
+        startElement(WEBDAV_NS_URI, WEBDAV_EL_DISPNAME, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_DISPNAME, null);
+        characters(displayName);
+        endElement(WEBDAV_NS_URI, WEBDAV_EL_DISPNAME, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_DISPNAME);
+        // lastmodified
+        startElement(WEBDAV_NS_URI, WEBDAV_EL_LASTMOD, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_LASTMOD, null);
+        characters(lastModified);
+        endElement(WEBDAV_NS_URI, WEBDAV_EL_LASTMOD, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_LASTMOD);
+        // creationdate
+        startElement(WEBDAV_NS_URI, WEBDAV_EL_CREATDATE, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_CREATDATE, null);
+        endElement(WEBDAV_NS_URI, WEBDAV_EL_CREATDATE, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_CREATDATE);
+        //resourcetype
+        startElement(WEBDAV_NS_URI, WEBDAV_EL_RESTYPE, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_RESTYPE, null);
+        if (type == WEBDAV_EL_COLL) {
+            // collection
+            startElement(WEBDAV_NS_URI, type, WEBDAV_NS_PREFIX + ":" + type, null);
+            endElement(WEBDAV_NS_URI, type, WEBDAV_NS_PREFIX + ":" + type);
+        }
+        endElement(WEBDAV_NS_URI, WEBDAV_EL_RESTYPE, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_RESTYPE);
+        // getcontenttype
+        startElement(WEBDAV_NS_URI, WEBDAV_EL_CONTTYPE, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_CONTTYPE, null);
+        characters(mimeType);
+        endElement(WEBDAV_NS_URI, WEBDAV_EL_CONTTYPE, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_CONTTYPE);
+        // getcontentlength
+        startElement(WEBDAV_NS_URI, WEBDAV_EL_CONTLEN, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_CONTLEN, null);
+        characters("0");
+        endElement(WEBDAV_NS_URI, WEBDAV_EL_CONTLEN, WEBDAV_NS_PREFIX + ":" +  WEBDAV_EL_CONTLEN);
+        // lockdiscovery
+        startElement(WEBDAV_NS_URI, WEBDAV_EL_LOCKDISC, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_LOCKDISC, null);
+        endElement(WEBDAV_NS_URI, WEBDAV_EL_LOCKDISC, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_LOCKDISC);            
+        endElement(WEBDAV_NS_URI, WEBDAV_EL_PROP, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_PROP);
+        // status
+        startElement(WEBDAV_NS_URI, WEBDAV_EL_STATUS, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_STATUS, null);
+        characters("HTTP/1.1 200 OK");
+        endElement(WEBDAV_NS_URI, WEBDAV_EL_STATUS, WEBDAV_NS_PREFIX + ":" +  WEBDAV_EL_STATUS);
+        endElement(WEBDAV_NS_URI, WEBDAV_EL_PROPSTAT, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_PROPSTAT);
+        endElement(WEBDAV_NS_URI, WEBDAV_EL_RESPONSE, WEBDAV_NS_PREFIX + ":" + WEBDAV_EL_RESPONSE);        
+    }
+    
+}
---
+++ src/contributions/resources/davcollection/src/java/org/wyona/yanel/impl/resources/XmlViewResource.java
@@ -0,0 +1,151 @@
+package org.wyona.yanel.impl.resources;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+import org.wyona.yanel.core.Path;
+import org.wyona.yanel.core.Resource;
+import org.wyona.yanel.core.api.attributes.ViewableV2;
+import org.wyona.yanel.core.attributes.viewable.View;
+import org.wyona.yanel.core.attributes.viewable.ViewDescriptor;
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+
+/**
+ * Abstract class that can be extended for resources that generate an XML based View.
+ * 
+ * @author greg
+ *      
+ * TODO find a way to let the view directly implement the ContentHandler interface
+ *      like generators in cocoon do.  
+ */
+public abstract class XmlViewResource extends Resource implements ViewableV2, ContentHandler {
+
+    // the XML content handler
+    protected ContentHandler contentHandler;
+    
+    // the XML serializer
+    protected XMLSerializer xmlSerializer;
+    
+    // XML mime-type
+    protected static final String XML_MIME_TYPE    = "application/xml";
+
+    // Attributes object
+    protected Attributes attributes;
+    
+    // XML serializer defaults
+    protected static final String DEFAULT_METHOD       = "XML";
+    protected static final boolean DEFAULT_INDENT      = true;
+    protected static final String DEFAULT_ENCODING     = "UTF-8"; 
+    protected static final int DEFAULT_INDENT_WIDTH    = 2;
+    protected static final int DEFAULT_LINE_WIDTH      = 120;
+    
+    public XmlViewResource() throws ParserConfigurationException, IOException {
+        super();
+
+        // set the output format values
+        OutputFormat outputFormat = new OutputFormat(DEFAULT_METHOD, DEFAULT_ENCODING, DEFAULT_INDENT);
+        outputFormat.setIndent(DEFAULT_INDENT_WIDTH);
+        outputFormat.setLineWidth(DEFAULT_LINE_WIDTH);
+        //outputFormat.setStandalone(true);
+        // initialise the XML content handler
+        this.xmlSerializer = new XMLSerializer(outputFormat);
+    }
+
+    /**
+     * Generates the actual view by working on the contentHandler.
+     * 
+     * @param path
+     * @param viewId
+     * @param requestParameters
+     * @throws SAXException 
+     */
+    public abstract void buildXmlView(Path path, String viewId, HttpServletRequest request, HttpServletResponse response) throws SAXException;
+        
+    public void getView(HttpServletRequest request, HttpServletResponse response, String viewId) throws Exception {
+        // set the output stream for the XML serializer
+        this.xmlSerializer.setOutputByteStream(response.getOutputStream());       
+        
+        this.contentHandler = xmlSerializer.asContentHandler();
+        
+        buildXmlView(new Path(request.getServletPath()), viewId, request, response);
+    }
+
+    public View getView(Path path, OutputStream outputStream, String viewId) throws Exception {
+        
+        // set the output stream for the XML serializer
+        this.xmlSerializer.setOutputByteStream(outputStream);
+        
+        this.contentHandler = xmlSerializer.asContentHandler();
+        
+        buildXmlView(path, viewId, null, null);
+        
+        return null;
+    }
+
+    public ViewDescriptor[] getViewDescriptors() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void characters(String arg0) throws SAXException {
+        char[] chars = arg0.toCharArray();
+        this.contentHandler.characters(chars, 0, chars.length);
+    }
+
+    public void characters(char[] arg0, int arg1, int arg2) throws SAXException {
+        this.contentHandler.characters(arg0, arg1, arg2);
+    }
+
+    public void endDocument() throws SAXException {
+        this.contentHandler.endDocument();
+    }
+
+    public void endElement(String arg0, String arg1, String arg2) throws SAXException {
+        this.contentHandler.endElement(arg0, arg1, arg2);
+    }
+
+    public void endPrefixMapping(String arg0) throws SAXException {
+        this.contentHandler.endPrefixMapping(arg0);
+    }
+
+    public void startDocument() throws SAXException {
+        this.contentHandler.startDocument();
+    }
+
+    public void startElement(String arg0, String arg1, String arg2, Attributes arg3) throws SAXException {
+        this.contentHandler.startElement(arg0, arg1, arg2, arg3);
+    }
+
+    public void startPrefixMapping(String arg0, String arg1) throws SAXException {
+        this.contentHandler.startPrefixMapping(arg0, arg1);
+    }
+    
+
+    public void setDocumentLocator(Locator arg0) {
+        this.contentHandler.setDocumentLocator(arg0);
+    }
+
+    public void ignorableWhitespace(char[] arg0, int arg1, int arg2) throws SAXException {
+        this.contentHandler.ignorableWhitespace(arg0, arg1, arg2);
+    }
+
+    public void processingInstruction(String arg0, String arg1) throws SAXException {
+        this.contentHandler.processingInstruction(arg0, arg1);
+    }
+
+    public void skippedEntity(String arg0) throws SAXException {
+        this.contentHandler.skippedEntity(arg0);
+    }
+    
+}
