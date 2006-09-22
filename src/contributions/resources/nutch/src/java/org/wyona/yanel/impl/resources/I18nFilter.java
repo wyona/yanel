e113e7ff80a95ea46c122723caaca2ae786dc9b9
david <david@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-09-22 15:16:11 +0000

    fixed problem with illegal state exception

--- src/contributions/resources/nutch/src/java/org/wyona/yanel/impl/resources/I18nFilter.java
+++ src/contributions/resources/nutch/src/java/org/wyona/yanel/impl/resources/I18nFilter.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 import org.apache.log4j.Category;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
 import org.xml.sax.helpers.XMLFilterImpl;
 
 public class I18nFilter extends XMLFilterImpl {
@@ -24,27 +25,28 @@ public class I18nFilter extends XMLFilterImpl {
             char[] i18nMessage = i18nText.toCharArray(); 
             try {
                 this.characters(i18nMessage, 0, i18nMessage.length);    
-            } catch(SAXException sax) {
-                sax.printStackTrace();
+            } catch(Exception e) {
+                log.error(e.getMessage(), e);
             }
         } else {
-            String values = "";
+            AttributesImpl attributes = new AttributesImpl(atts);
             for(int i = 0; i < atts.getLength(); i++) {
                 String value = atts.getValue(i);
-                String name = atts.getLocalName(i);
                 if(value.indexOf("i18n:attr key=") != -1) {
                     String key = value.substring(14);
-                    value = messageBundle.getString(key);
-                    log.error("ATTR [key] " + key + " [message]" + value);
+                    String i18nValue = messageBundle.getString(key);
+                    attributes.setAttribute(i, uri, localName, qName, "String", i18nValue);
+                    log.error("ATTR [key] " + key + " [message]" + i18nValue);
+                } else {
+                    attributes.setAttribute(i, uri, localName, qName, "String", value);
                 }
-                values += name + "=" + value + " ";
             }
             try {
-                this.characters(values.toCharArray(), 0, values.length());    
-            } catch(SAXException sax) {
-                sax.printStackTrace();
+                super.startElement(uri, localName, qName, attributes);    
+            } catch(Exception e) {
+                log.error(e.getMessage(), e);
             }
-        } 
+        }
     }
 
     public void endElement(String uri, String localName, String qName) throws SAXException {
@@ -53,5 +55,4 @@ public class I18nFilter extends XMLFilterImpl {
             super.endElement(uri, localName, qName);
         }
     }
-
 }
