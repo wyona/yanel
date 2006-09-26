af8befe99f4f1b3d4c6d79a0f9255e6f32faa503
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-09-26 15:35:51 +0000

    Yanel started

---
+++ src/core/java/org/wyona/yanel/core/Yanel.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2006 Wyona
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.wyona.org/licenses/APACHE-LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.wyona.yanel.core;
+
+import org.wyona.yanel.core.map.Map;
+import org.wyona.yanel.core.map.MapFactory;
+
+/**
+ *
+ */
+public class Yanel {
+
+    Map map = null;
+    ResourceTypeRegistry rtr = null;
+
+    /**
+     *
+     */
+    public Yanel() {
+        MapFactory mf = MapFactory.newInstance();
+        map = mf.newMap();
+        rtr = new ResourceTypeRegistry();
+    }
+
+    /**
+     *
+     */
+    public Resource getResource(Path path) throws Exception {
+        String rti = map.getResourceTypeIdentifier(path);
+        return rtr.newResource(rti);
+    }
+}
