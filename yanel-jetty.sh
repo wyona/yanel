6b07ec2278d753b3ff71750338246a28e907e9a5
thorsten <thorsten@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-09-27 15:40:35 +0000

    Adding jetty support to yanel. Make sure you have build first. if you need to override the war location then use the script like "-Dyanel.war.location=..."

---
+++ yanel-jetty.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo "Start/Stop Yanel ..."
+
+# ----- Check for JAVA_HOME
+JAVA_HOME="$JAVA_HOME"
+if [ "$JAVA_HOME" = "" ];then
+  echo "ERROR: No JAVA_HOME set!"
+  echo "       Have you installed JDK (Java Development Kit)? If so, then set JAVA_HOME ..."
+  echo "       MacOS X : setenv JAVA_HOME /usr"
+  echo "       Linux   : export JAVA_HOME=/usr/local/j2sdk-..."
+  echo "       Windows : Click Start ..."
+  exit 1
+fi
+
+# ----- Set Environment Variables
+ORIGINAL_ANT_HOME=$ANT_HOME
+unset ANT_HOME
+ANT_HOME=$PWD/tools/apache-ant-1.6.2
+#echo $ANT_HOME
+
+ORIGINAL_PATH=$PATH
+PATH=$PWD/tools/maven-2.0.4/bin:$ANT_HOME/bin:$PATH
+#echo $PATH
+
+# ----- Build Yanel ...
+mvn --version
+mvn jetty:run-war -f src/build/pom-webapp.xml $@
