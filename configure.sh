d494264bc17e42182e0210b5811c00584d07ab5b
thorsten <thorsten@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-09-27 09:54:04 +0000

    Adding configure targets that will create all local.*.properties files if not exit. Refactoring configure.sh to work with the configure target

--- configure.sh
+++ configure.sh
@@ -31,18 +31,7 @@ PATH=$PWD/tools/maven-2.0.4/bin:$ANT_HOME/bin:$PATH
 # ----- Do something ;-)
 ant -version
 #mvn --version
-
-echo ""
-echo "NOTE 0: As a first time user one does NOT have to configure anything, but just run build.sh and then yanel.sh! :-)"
-echo ""
-
-echo ""
-echo "NOTE 1: Copy src/build/build.properties to src/build/local.build.properties and customize the local.build.properties file."
-echo ""
-
-echo ""
-echo "NOTE 2: Copy conf/yanel.xml to local.yanel.xml, copy conf/realms.xml to conf/local.realms.xml, copy conf/yanel-rti-yarep.properties to conf/local.yanel-rti-yarep.properties, copy conf/yarep.properties to conf/local.yarep.properties, copy conf/ac-policies-yarep.properties to conf/local.ac-policies-yarep.properties and copy conf/ac-identities-yarep.properties to conf/local.ac-identities-yarep.properties. Customize the local.*.properties files and run build.sh! Or run build.sh first and then customize the *.properties files within build/webapps/yanel/WEB-INF/classes"
-echo ""
+ant -f src/build/build.xml config
 
 # ----- Reset Environment Variables
 ANT_HOME=$ORIGINAL_ANT_HOME
@@ -51,5 +40,5 @@ PATH=$ORIGINAL_PATH
 #echo $PATH
 
 echo "INFO: Configuration has been completed successfully :-)"
-echo ""
+echo "INFO: Remember to customize the properties files if needed."
 echo "INFO: Start building Yanel by executing build.sh"
