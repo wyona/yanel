6bfbda61cb61c8f826b6dca0368d340b78998ccc
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-10-03 21:39:21 +0000

    add yanel.path argument

--- yanel.bat
+++ yanel.bat
@@ -5,9 +5,8 @@ set ORIGINAL_ANT_HOME=%ANT_HOME%
 set ANT_HOME=tools\apache-ant-1.6.5
 
 call %ANT_HOME%\bin\ant -version
-rem call %ANT_HOME%\bin\ant -f src\build\build.xml
-call %ANT_HOME%\bin\ant -f src\build\build.xml run-yanel-cmdl
-rem call %ANT_HOME%\bin\ant -f src\build\build.xml -Djava.endorsed.dirs=lib\endorsed -logger org.apache.tools.ant.NoBannerLogger -emacs run-yanel-cmdl
+rem call %ANT_HOME%\bin\ant -f src\build\build.xml run-yanel-cmdl -Dyanel.path=%1
+call %ANT_HOME%\bin\ant -f src\build\build.xml run-yanel-cmdl -Dyanel.path=""
 
 rem ----- Restore ANT_HOME
 set ANT_HOME=%ORIGINAL_ANT_HOME%
