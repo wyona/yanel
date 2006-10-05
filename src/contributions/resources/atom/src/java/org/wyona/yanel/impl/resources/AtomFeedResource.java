f9a19d1c520094e9a6ccbac03dbef5c0053c0c51
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-10-05 23:06:50 +0000

    perma link started

--- src/contributions/resources/atom/src/java/org/wyona/yanel/impl/resources/AtomFeedResource.java
+++ src/contributions/resources/atom/src/java/org/wyona/yanel/impl/resources/AtomFeedResource.java
@@ -97,6 +97,7 @@ public class AtomFeedResource extends Resource implements ViewableV1 {
                                 Entry entry = (Entry) doc.getRoot();
                                 if (entry != null) {
                                     entry.addLink(children[i].getName(), "edit");
+                                    //entry.addLink("http://www.yulup.org:80/news-entries" + children[i].getName());
                                     orderedEntries = addEntry(orderedEntries, entry);
                                 } else {
                                     log.error("Atom entry is null!" + children[i]);
