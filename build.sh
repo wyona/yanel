fede0c761e56d38c35ce1ffe51ee5436f8c793a2
simon <simon@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-11-02 10:52:52 +0000

    patch from andreas wuest. thanks a lot!
    * If JAVA_HOME is not set, the shell script gives bad advice for MacOS X users. JAVA_HOME should be set to /System/Library/Frameworks/JavaVM.framework/Home.
    * Removed the saving of original paths and their restoration. Since the script runs in a subshell (/bin/sh), changing environment variables does not affect parent shells. Furthermore, it was not done consistently anyway.

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ JAVA_HOME="$JAVA_HOME"
 if [ "$JAVA_HOME" = "" ];then
   echo "ERROR: No JAVA_HOME set!"
   echo "       Have you installed JDK (Java Development Kit)? If so, then set JAVA_HOME ..."
-  echo "       MacOS X : setenv JAVA_HOME /usr"
+  echo "       MacOS X : setenv JAVA_HOME /System/Library/Frameworks/JavaVM.framework/Home"
   echo "       Linux   : export JAVA_HOME=/usr/local/j2sdk-..."
   echo "       Windows : Click Start ..."
   exit 1
@@ -19,22 +19,14 @@ fi
 # TODO: ....
 
 # ----- Set Environment Variables
-ORIGINAL_ANT_HOME=$ANT_HOME
 unset ANT_HOME
 ANT_HOME=$PWD/tools/apache-ant-1.6.5
 #echo $ANT_HOME
 
-ORIGINAL_PATH=$PATH
 PATH=$PWD/tools/maven-2.0.4/bin:$ANT_HOME/bin:$PATH
 #echo $PATH
 
-# ----- Build Yanel ...
+# ----- Build Yanel
 #mvn --version
 ant -version
 ant -f src/build/build.xml $@
-
-# ----- Reset Environment Variables
-ANT_HOME=$ORIGINAL_ANT_HOME
-#echo $ANT_HOME
-PATH=$ORIGINAL_PATH
-#echo $PATH
--- yanel.sh
+++ yanel.sh
@@ -7,23 +7,21 @@ JAVA_HOME="$JAVA_HOME"
 if [ "$JAVA_HOME" = "" ];then
   echo "ERROR: No JAVA_HOME set!"
   echo "       Have you installed JDK (Java Development Kit)? If so, then set JAVA_HOME ..."
-  echo "       MacOS X : setenv JAVA_HOME /usr"
+  echo "       MacOS X : setenv JAVA_HOME /System/Library/Frameworks/JavaVM.framework/Home"
   echo "       Linux   : export JAVA_HOME=/usr/local/j2sdk-..."
   echo "       Windows : Click Start ..."
   exit 1
 fi
 
 # ----- Set Environment Variables
-ORIGINAL_ANT_HOME=$ANT_HOME
 unset ANT_HOME
 ANT_HOME=$PWD/tools/apache-ant-1.6.5
 #echo $ANT_HOME
 
-ORIGINAL_PATH=$PATH
 PATH=$PWD/tools/maven-2.0.4/bin:$ANT_HOME/bin:$PATH
 #echo $PATH
 
-# ----- Build Yanel ...
+# ----- Start/Stop Yanel
 #mvn --version
 ant -version
 ant -f src/build/build.xml run-yanel-cmdl -Dyanel.path=$1
