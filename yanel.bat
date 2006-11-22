a833494cbf26dd3fac0a278b6e9d74fb4cd42b88
simon <simon@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-11-22 08:35:10 +0000

    yanel.bat (windows) is now able to start and stop tomcat. bug#5042. thanks to ilhan!

--- yanel.bat
+++ yanel.bat
@@ -1,5 +1,23 @@
 @echo off
 
+echo "Start/Stop Yanel ..."
+
+:: check if JAVA_HOME is set or goto end
+if not "%JAVA_HOME%" == "" goto gotJavaHome
+echo You must set JAVA_HOME to point at your Java Development Kit installation
+goto end
+:gotJavaHome
+::----- Start/Stop Yanel
+if "%1" == "start"; goto start
+if "%1" == "stop"; goto stop
+
+:start
+  ant -f src/build/build.xml start-tomcat
+:stop
+  ant -f src/build/build.xml stop-tomcat
+else
+  ant -f src/build/build.xml run-yanel-cmdl -Dyanel.path=%1
+  
 rem ----- Ignore system ANT_HOME variable
 set ORIGINAL_ANT_HOME=%ANT_HOME%
 set ANT_HOME=tools\apache-ant-1.6.5
