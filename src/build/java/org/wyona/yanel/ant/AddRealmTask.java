e207d4f698ab559627e1f6d72494187aaac98ce6
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-12-08 08:35:56 +0000

    add realm target started

---
+++ src/build/java/org/wyona/yanel/ant/AddRealmTask.java
@@ -0,0 +1,23 @@
+/*
+ *
+ */
+package org.wyona.yanel.ant;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+import org.wyona.yanel.core.map.RealmConfiguration;
+
+/**
+ *
+ */
+public class AddRealmTask extends Task {
+
+    /**
+     *
+     */
+    public void execute() throws BuildException {
+        log("Not implemented yet!");
+        //RealmConfiguration rc = new RealmConfiguration();
+    }
+}
