3a7ea7b1afd2034ca9a83fe67ab904bffd4ba8df
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-12-10 13:15:10 +0000

    resource minimals added

---
+++ src/resources/from-scratch-resource-template/src/java/bar/foo/yanel/impl/resources/FromScratchResource.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2006 Wyona
+ */
+
+package bar.foo.yanel.impl.resources;
+
+import org.wyona.yanel.impl.ResourceDefaultImpl;
+
+/**
+ *
+ */
+public class FromScratchResource extends ResourceDefaultImpl {
+
+    /**
+     *
+     */
+    public FromScratchResource() {
+    }
+}
