f0f072a4a9c1b3286a20f2d7d4c9970372867d24
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-12-28 15:23:32 +0000

    aggregate events

--- src/contributions/resources/calendar/src/java/org/wyona/yanel/impl/resources/CalendarResource.java
+++ src/contributions/resources/calendar/src/java/org/wyona/yanel/impl/resources/CalendarResource.java
@@ -46,28 +46,37 @@ public class CalendarResource extends Resource implements ViewableV2 {
     public View getView(String viewId) throws Exception {
         org.wyona.yarep.core.Repository dataRepo = getRealm().getRepository();
         org.wyona.yarep.core.Path[] children = dataRepo.getChildren(new org.wyona.yarep.core.Path(getPath().toString()));
+
+        StringBuffer calendar = new StringBuffer("<?xml version=\"1.0\"?>\n<calendar>");
         for (int i = 0; i < children.length; i++) {
             log.error("DEBUG: Child: " + children[i]);
             if (dataRepo.isResource(children[i])) {
-            java.io.InputStream in = dataRepo.getInputStream(children[i]);
-            StringBuffer event = new StringBuffer();
+                java.io.InputStream in = dataRepo.getInputStream(children[i]);
+                StringBuffer event = new StringBuffer();
             
-            byte[] buffer = new byte[8192];
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) != -1) {
-                event.append(new String(buffer));
-            }
+                byte[] buffer = new byte[8192];
+                int bytesRead;
+                while ((bytesRead = in.read(buffer)) != -1) {
+                    event.append(new String(buffer));
+                }
 
-            log.error("DEBUG: Event: " + event);
+                log.error("DEBUG: Event: " + event);
+                int endOfProcessingInstruction = event.indexOf("?>");
+                if (endOfProcessingInstruction > 0) {
+                    calendar.append(event.toString().substring(endOfProcessingInstruction + 2));
+                } else {
+                    log.error("No processing instruction: " + children[i]);            
+                }
             }
         }
+        calendar.append("</calendar>");
 
         //response.getOutputStream();
 
         View view = new View();
         //view.setResponse(false);
-	view.setMimeType("text/plain");
-	view.setInputStream(new java.io.StringBufferInputStream("Hello Calendar"));
+	view.setMimeType("application/xml");
+	view.setInputStream(new java.io.StringBufferInputStream(calendar.toString()));
         return view;
     }
 
