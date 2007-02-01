2945730baa127fd1176cb433e065288cd7cf1ca2
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-02-01 08:25:49 +0000

    windows scripts added

---
+++ src/binary-dist/stop.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo "Stop Yanel ..."
+
+:: Check if JAVA_HOME is set or goto end
+if not "%JAVA_HOME%" == "" goto gotJavaHome
+echo You must set JAVA_HOME to point at your Java Development Kit installation
+goto end
+:gotJavaHome
+
+apache-tomcat-5.5.20\bin\shutdown.bat
