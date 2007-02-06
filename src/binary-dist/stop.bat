20b9e37d40089298206dff6f0d0e7644a6279b67
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-02-06 23:09:21 +0000

    CATALINA_HOME fixed

--- src/binary-dist/stop.bat
+++ src/binary-dist/stop.bat
@@ -2,10 +2,21 @@
 
 echo "Stop Yanel ..."
 
+rem ----- Ignore system CATALINA_HOME variable
+set ORIGINAL_CATALINA_HOME=%CATALINA_HOME%
+set CATALINA_HOME=apache-tomcat-5.5.20
+
 :: Check if JAVA_HOME is set or goto end
 if not "%JAVA_HOME%" == "" goto gotJavaHome
 echo You must set JAVA_HOME to point at your Java Development Kit installation
 goto end
+
 :gotJavaHome
 
-apache-tomcat-5.5.20\bin\shutdown.bat
+%CATALINA_HOME%\bin\shutdown.bat
+
+rem ----- Restore CATALINA_HOME
+set CATALINA_HOME=%ORIGINAL_CATALINA_HOME%
+set ORIGINAL_CATALINA_HOME=
++
++:end
