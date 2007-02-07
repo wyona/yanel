7fffaeff1d38ee8cb61a5e1e49927fcc80b86c35
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-02-07 10:01:17 +0000

    formatting fixed

--- src/resources/directory/src/java/org/wyona/yanel/impl/resources/DirectoryResource.java
+++ src/resources/directory/src/java/org/wyona/yanel/impl/resources/DirectoryResource.java
@@ -101,10 +101,7 @@ public class DirectoryResource extends Resource implements ViewableV2 {
             // TODO: Add realm prefix, e.g. realm-prefix="ulysses-demo"
             // NOTE: The schema is according to
             // http://cocoon.apache.org/2.1/userdocs/directory-generator.html
-            sb.append("<dir:directory yanel:path=\"" + getPath() + "\"
-			              dir:name=\"" + p.getName() + "\"
-				      dir:path=\"" + p + "\"
-				      xmlns:dir=\"http://apache.org/cocoon/directory/2.0\" xmlns:yanel=\"http://www.wyona.org/yanel/resource/directory/1.0\">");
+            sb.append("<dir:directory yanel:path=\"" + getPath() + "\" dir:name=\"" + p.getName() + "\" dir:path=\"" + p + "\" xmlns:dir=\"http://apache.org/cocoon/directory/2.0\" xmlns:yanel=\"http://www.wyona.org/yanel/resource/directory/1.0\">");
             // TODO: Do not show the children with suffix .yanel-rti resp. make
             // this configurable!
             // NOTE: Do not hardcode the .yanel-rti, but rather use
