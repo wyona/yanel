0e62c1e9c74cd75e4e218f18e516e178711b9eb3
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-03-14 16:14:46 +0000

    introspection interface added

---
+++ src/core/java/org/wyona/yanel/core/api/attributes/IntrospectableV1.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2006 Wyona
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.wyona.org/licenses/APACHE-LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.wyona.yanel.core.api.attributes;
+
+/**
+ * DEV (not released yet), please be aware that this interface still might change ...
+ */
+public interface IntrospectableV1 {
+
+    /**
+     * Gets an xml which contains the neutron introspection xml.
+     * @return introspection xml as a string
+     * @throws Exception
+     */
+    public String getIntrospection() throws Exception;
+    
+}
