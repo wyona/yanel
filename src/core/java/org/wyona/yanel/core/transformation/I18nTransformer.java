14624e80ae5ad6d98860c256ab0670901282e38a
josias <josias@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-03-27 21:24:39 +0000

    marked as deprecated

--- src/core/java/org/wyona/yanel/core/transformation/I18nTransformer.java
+++ src/core/java/org/wyona/yanel/core/transformation/I18nTransformer.java
@@ -13,6 +13,9 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+/**
+ * @deprecated please use I18nTransformer2 instead
+ */
 public class I18nTransformer extends DefaultHandler {
 
     private static Category log = Category.getInstance(I18nTransformer.class);
