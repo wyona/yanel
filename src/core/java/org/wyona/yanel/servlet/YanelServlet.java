b7cec59768f3ebc64dd4cee3d12cfda3f0223251
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-04-04 12:35:26 +0000

    links added

--- src/core/java/org/wyona/yanel/servlet/YanelServlet.java
+++ src/core/java/org/wyona/yanel/servlet/YanelServlet.java
@@ -1801,7 +1801,7 @@ public class YanelServlet extends HttpServlet {
         sb.append("<ul><li>");
 
         sb.append("<div id=\"yaneltoolbar_menutitle\">File</div><ul>");
-        sb.append("<li><a href=\"" + backToRealm + "create-new-page.html\">New</a><ul><li>Standard page (XHTML)</li><li>Wiki page</li></ul></li>");
+        sb.append("<li class=\"haschild\"><a href=\"" + backToRealm + "create-new-page.html\">New</a><ul><li><a href=\"" + backToRealm + "create-new-page.html?resource-type=http%3A%2F%2Fwww.wyona.org%2Fyanel%2Fresource%2F1.0%3A%3Axml\">Standard page (XHTML)</a></li><li><a href=\"" + backToRealm + "create-new-page.html?resource-type=http%3A%2F%2Fwww.wyona.org%2Fyanel%2Fresource%2F1.0%3A%3Awiki\">Wiki page</a></li></ul></li>");
         sb.append("<li class=\"haschild\">New language<ul><li>German</li><li>Mandarin</li></ul></li>");
         sb.append("<li>Publish</li>");
         sb.append("</ul></li></ul>");
