8b1c7affd9a008a5793cd5ba25a29283610cd4fe
simon <simon@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-04-24 08:49:04 +0000

    formatting fixed

--- src/realms/welcome-admin/yanel/resources/update-webapp/src/java/org/wyona/yanel/impl/resources/UpdateResource.java
+++ src/realms/welcome-admin/yanel/resources/update-webapp/src/java/org/wyona/yanel/impl/resources/UpdateResource.java
@@ -42,11 +42,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 
-
-
 import com.hp.hpl.jena.rdf.model.*;
-
-
 
 /**
  * 
@@ -56,7 +52,7 @@ public class UpdateResource extends Resource implements ViewableV2 {
     private static Category log = Category.getInstance(UpdateResource.class);
     private String defaultLanguage;
     private String language = null;
-    
+
     /**
      * 
      */
@@ -95,49 +91,46 @@ public class UpdateResource extends Resource implements ViewableV2 {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         View defaultView = new View();
         DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        
+
         try {
 
-                    //install.rdf
-                    
-                    InstallInfo installInfo = new InstallInfo(request);
+            // install.rdf
 
-                    //just testing
-                    System.out.println(installInfo.getId());
-                    System.out.println(installInfo.getInstalltype());
-                    System.out.println(installInfo.getContextPrefix());
-                    System.out.println(installInfo.getOsName());
-                    System.out.println(installInfo.getTargetApplicationId());
-                    System.out.println(installInfo.getTargetApplicationVersion());
-                    System.out.println(installInfo.getUpdateURL());
-                    System.out.println(installInfo.getVersion());
-                    System.out.println(installInfo.getJavaVersion());
-                    for (int i = 0; i < installInfo.getProtectedFiles().size(); i++) {
-                        System.out.println("getProtectedFiles --------" + installInfo.getProtectedFiles().get(i));
-                    }
-                    
-                    
-                    
-                    URL UpdateRdfUrl = new URL(installInfo.getUpdateURL());
-                    InputStream updateRdfIn = UpdateRdfUrl.openStream();
-                    UpdateInfo updateInfo = new UpdateInfo(updateRdfIn, installInfo);
-                    
+            InstallInfo installInfo = new InstallInfo(request);
 
-                    
-                    if (installInfo.getInstalltype().equals("source")) {
-                        StringBuffer message = new StringBuffer();
-                        message.append("<p>");
-                        message.append("This Yanel was installed from source. You can only use the updater if you installed yanel from binary. Please use svn up, build.sh");
-                        message.append("</p>");
-                        
-                        byteArrayOutputStream = getOutput(message);
-                        //transformer = TransformerFactory.newInstance().newTransformer();
+            // just testing
+            System.out.println(installInfo.getId());
+            System.out.println(installInfo.getInstalltype());
+            System.out.println(installInfo.getContextPrefix());
+            System.out.println(installInfo.getOsName());
+            System.out.println(installInfo.getTargetApplicationId());
+            System.out.println(installInfo.getTargetApplicationVersion());
+            System.out.println(installInfo.getUpdateURL());
+            System.out.println(installInfo.getVersion());
+            System.out.println(installInfo.getJavaVersion());
+            for (int i = 0; i < installInfo.getProtectedFiles().size(); i++) {
+                System.out.println("getProtectedFiles --------"
+                        + installInfo.getProtectedFiles().get(i));
+            }
 
-                        
-                    } else if (installInfo.getInstalltype().equals("bin-snapshot")) {
-                if (request.getParameter("update") != null && request.getParameter("update").equals("true")) {
+            URL UpdateRdfUrl = new URL(installInfo.getUpdateURL());
+            InputStream updateRdfIn = UpdateRdfUrl.openStream();
+            UpdateInfo updateInfo = new UpdateInfo(updateRdfIn, installInfo);
+
+            if (installInfo.getInstalltype().equals("source")) {
+                StringBuffer message = new StringBuffer();
+                message.append("<p>");
+                message.append("This Yanel was installed from source. You can only use the updater if you installed yanel from binary. Please use svn up, build.sh");
+                message.append("</p>");
+
+                byteArrayOutputStream = getOutput(message);
+                // transformer = TransformerFactory.newInstance().newTransformer();
+
+            } else if (installInfo.getInstalltype().equals("bin-snapshot")) {
+                if (request.getParameter("update") != null
+                        && request.getParameter("update").equals("true")) {
                     WebAppUpdater webAppUpdater = new WebAppUpdater(request, "");
-                    
+
                     StringBuffer message = new StringBuffer();
                     message.append("<p>");
                     if (webAppUpdater.update()) {
@@ -154,35 +147,41 @@ public class UpdateResource extends Resource implements ViewableV2 {
                     message.append("</p>");
                     message.append("<ul>");
                     for (int i = 0; i < updateInfo.getUpdateVersions().size(); i++) {
-                        HashMap versionDetails = (HashMap) updateInfo.getUpdateVersions()
-                                .get(i);
+                        HashMap versionDetails = (HashMap) updateInfo.getUpdateVersions().get(i);
                         if (versionDetails.get("version") != installInfo.getVersion()) {
-                            message.append("<li>Version: " + versionDetails.get("version") + " ChangeLog: "
-                                    + versionDetails.get("changeLog") + " Update Link: " + versionDetails.get("updateLink") + "<form><input type=\"submit\" name=\"update\" value=\"true\"></input></form></li>");
+                            message.append("<li>Version: "
+                                    + versionDetails.get("version")
+                                    + " ChangeLog: "
+                                    + versionDetails.get("changeLog")
+                                    + " Update Link: "
+                                    + versionDetails.get("updateLink")
+                                    + "<form><input type=\"submit\" name=\"update\" value=\"true\"></input></form></li>");
                         }
                     }
                     message.append("</ul>");
                     byteArrayOutputStream = getOutput(message);
                 }
             }
-                    // this.installRdf.put("updateURL",
-                    // installRdf.getChild("Description").getChild("um:updateURL").getValue());
-                    //this.installRdf.put("version", installRdf.getChild("Description").getChild("um:version").getValue()); 
-                    //transformer.transform(new StreamSource(InstallRdf), new StreamResult(byteArrayOutputStream));
-                    
-                    //update.rdf
-                    //URL UpdateRdfUrl = new URL(installRdf.getUpdateURL());
-                    //InputStream UpdateRdf = UpdateRdfUrl.openStream();
-                    
-                    //Configuration updateRdf = configBuilder.build(UpdateRdf);
-                    //Document UpdateRdfDoc = builder.parse(UpdateRdf);
-                    //transformer = TransformerFactory.newInstance().newTransformer();
-                    //transformer.transform(new DOMSource(UpdateRdfDoc), new StreamResult(byteArrayOutputStream));
-                    
-                    
+            // this.installRdf.put("updateURL",
+            // installRdf.getChild("Description").getChild("um:updateURL").getValue());
+            // this.installRdf.put("version",
+            // installRdf.getChild("Description").getChild("um:version").getValue());
+            // transformer.transform(new StreamSource(InstallRdf), new
+            // StreamResult(byteArrayOutputStream));
 
-            //SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
-            //saxParser.parse(new ByteArrayInputStream(byteArrayOutputStream.toByteArray()),i18nTransformer);
+            // update.rdf
+            // URL UpdateRdfUrl = new URL(installRdf.getUpdateURL());
+            // InputStream UpdateRdf = UpdateRdfUrl.openStream();
+
+            // Configuration updateRdf = configBuilder.build(UpdateRdf);
+            // Document UpdateRdfDoc = builder.parse(UpdateRdf);
+            // transformer = TransformerFactory.newInstance().newTransformer();
+            // transformer.transform(new DOMSource(UpdateRdfDoc), new
+            // StreamResult(byteArrayOutputStream));
+
+            // SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
+            // saxParser.parse(new
+            // ByteArrayInputStream(byteArrayOutputStream.toByteArray()),i18nTransformer);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
@@ -192,7 +191,8 @@ public class UpdateResource extends Resource implements ViewableV2 {
         return defaultView;
     }
 
-    private ByteArrayOutputStream getOutput(StringBuffer insert) throws TransformerConfigurationException {
+    private ByteArrayOutputStream getOutput(StringBuffer insert)
+            throws TransformerConfigurationException {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         StringBuffer sb = new StringBuffer();
         sb.append("<?xml version=\"1.0\"?>");
@@ -204,16 +204,17 @@ public class UpdateResource extends Resource implements ViewableV2 {
         sb.append(insert);
         sb.append("</body>");
         sb.append("</html>");
-        
+
         try {
             Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            transformer.transform(new StreamSource(new java.io.StringBufferInputStream(sb.toString())), new StreamResult(byteArrayOutputStream));
+            transformer.transform(new StreamSource(new java.io.StringBufferInputStream(sb.toString())),
+                    new StreamResult(byteArrayOutputStream));
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
         return byteArrayOutputStream;
     }
-    
+
     public boolean exists() throws Exception {
         // NOTE does exists() make sense for this resource?
         log.warn("Not implemented yet!");
@@ -227,17 +228,18 @@ public class UpdateResource extends Resource implements ViewableV2 {
         // NOTE does getSize make sense for this resource?
         return getRealm().getRepository().getSize(new Path(getPath()));
     }
-    
+
     /**
      * Get property value from resource configuration
      */
     private String getResourceProperty(String name) throws Exception {
         ResourceConfiguration rc = getConfiguration();
-        if (rc != null) return rc.getProperty(name);
+        if (rc != null)
+            return rc.getProperty(name);
         return getRTI().getProperty(name);
     }
-    
-    public String getMimeType(String viewId){
+
+    public String getMimeType(String viewId) {
         return "application/xml";
     }
 }
