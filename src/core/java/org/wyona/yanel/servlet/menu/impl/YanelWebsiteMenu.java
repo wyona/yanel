8e9a05d04bbe07d4bc6eded41ac6ea718981ca4a
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-05-02 09:43:47 +0000

    check for versionable interface

--- src/core/java/org/wyona/yanel/servlet/menu/impl/YanelWebsiteMenu.java
+++ src/core/java/org/wyona/yanel/servlet/menu/impl/YanelWebsiteMenu.java
@@ -60,7 +60,10 @@ public class YanelWebsiteMenu extends Menu {
         sb.append("<ul><li>");
         sb.append("<div id=\"yaneltoolbar_menutitle\">Edit</div><ul>");
         sb.append("<li class=\"haschild\">Open with<ul><li>Source editor</li><li>WYSIWYG editor</li></ul></li>");
-        sb.append("<li class=\"haschild\">Revisions<ul><li class=\"haschild\">53534<ul><li>Show diff</li><li>Revert to</li></ul></li><li class=\"haschild\">52108<ul><li>Show diff</li><li>Revert to</li></ul></li></ul></li>");
+
+        if (ResourceAttributeHelper.hasAttributeImplemented(resource, "Versionable", "2")) {
+            sb.append("<li class=\"haschild\">Revisions<ul><li class=\"haschild\">53534<ul><li>Show diff</li><li>Revert to</li></ul></li><li class=\"haschild\">52108<ul><li>Show diff</li><li>Revert to</li></ul></li></ul></li>");
+        }
         sb.append("</ul>");
         sb.append("</li></ul>");
 
