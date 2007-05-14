a4c58f57b4dbbb619868841030d6bd0f7a51fccf
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-05-14 21:28:36 +0000

    get properties methods are obsolete

--- src/resources/translation/src/java/org/wyona/yanel/impl/resources/TranslationResource.java
+++ src/resources/translation/src/java/org/wyona/yanel/impl/resources/TranslationResource.java
@@ -301,25 +301,6 @@ public class TranslationResource extends Resource implements ViewableV2 {
     }
     
     /**
-     * Get property value from resource configuration
-     */
-    private String getResourceConfigProperty(String name) throws Exception {
-    	ResourceConfiguration rc = getConfiguration();
-    	if (rc != null) return rc.getProperty(name);
-    	return getRTI().getProperty(name);
-    }
-    
-    /**
-     * Get property value from resource configuration
-     */
-    private String[] getResourceConfigProperties(String name) throws Exception {
-    	ResourceConfiguration rc = getConfiguration();
-    	if (rc != null) return rc.getProperties(name);
-    	return getRTI().getProperties(name);
-    }
-
-
-    /**
      *
      */
     public String getPropertyType(String name) {
