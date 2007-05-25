c87de2025dd4121f8d4cf2f7c61b698eae544b3a
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-05-25 12:50:20 +0000

    for testing added

---
+++ src/realms/yanel-website/src/java/org/wyona/yanel/servlet/menu/impl/README.txt
@@ -0,0 +1,1 @@
+Not used yet. See src/webapp/src/java/org/wyona/yanel/servlet/menu/impl/YanelWebsiteMenu.java
---
+++ src/realms/yanel-website/src/java/org/wyona/yanel/servlet/menu/impl/YanelWebsiteMenu.java
@@ -0,0 +1,81 @@
+package org.wyona.yanel.servlet.menu.impl;
+
+import org.wyona.yanel.core.Resource;
+import org.wyona.yanel.core.api.attributes.TranslatableV1;
+import org.wyona.yanel.core.api.attributes.VersionableV2;
+import org.wyona.yanel.core.attributes.versionable.RevisionInformation;
+import org.wyona.yanel.core.map.Map;
+import org.wyona.yanel.core.map.Realm;
+import org.wyona.yanel.core.util.ResourceAttributeHelper;
+
+import org.wyona.yanel.servlet.YanelServlet;
+import org.wyona.yanel.servlet.menu.Menu;
+
+import org.wyona.security.core.api.Identity;
+import org.wyona.security.core.api.IdentityMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ */
+public class YanelWebsiteMenu extends Menu {
+
+    /**
+     * Get toolbar menus
+     */
+    public  String getMenus(Resource resource, HttpServletRequest request, Map map, String reservedPrefix) throws ServletException, IOException, Exception {
+        String backToRealm = org.wyona.yanel.core.util.PathUtil.backToRealm(resource.getPath());
+        StringBuffer sb= new StringBuffer();
+
+	sb.append("<ul><li>");
+        sb.append("<div id=\"yaneltoolbar_menutitle\">File</div>");
+        sb.append("<ul>");
+        sb.append("<li class=\"haschild\"><a href=\"" + backToRealm + "create-new-page.html\">New&#160;&#160;&#160;</a><ul><li><a href=\"" + backToRealm + "create-new-page.html?resource-type=http%3A%2F%2Fwww.wyona.org%2Fyanel%2Fresource%2F1.0%3A%3Axml\">Standard page (XHTML)</a></li><li><a href=\"" + backToRealm + "create-new-page.html?resource-type=http%3A%2F%2Fwww.wyona.org%2Fyanel%2Fresource%2F1.0%3A%3Awiki\">Wiki page</a></li></ul></li>");
+        sb.append("<li class=\"haschild\">New language&#160;&#160;&#160;<ul>");
+        
+        if (ResourceAttributeHelper.hasAttributeImplemented(resource, "Translatable", "1")) {
+            TranslatableV1 translatable = (TranslatableV1)resource;
+            List existingLanguages = Arrays.asList(translatable.getLanguages());
+            String[] realmLanguages = resource.getRealm().getLanguages();
+            for (int i = 0; i < realmLanguages.length; i++) {
+                if (!existingLanguages.contains(realmLanguages[i])) {
+                    sb.append("<li>");
+                    sb.append(realmLanguages[i]);
+                    sb.append("</li>");
+                }
+            }
+        }
+        //sb.append("<li>German</li><li>Mandarin</li>");
+        sb.append("</ul></li>");
+        sb.append("<li>Publish</li>");
+        sb.append("</ul>");
+        sb.append("</li></ul>");
+
+        sb.append("<ul><li>");
+        sb.append("<div id=\"yaneltoolbar_menutitle\">Edit</div><ul>");
+        sb.append("<li class=\"haschild\">Open with&#160;&#160;&#160;<ul><li>Source editor</li><li>WYSIWYG editor</li></ul></li>");
+
+        if (ResourceAttributeHelper.hasAttributeImplemented(resource, "Versionable", "2")) {
+            RevisionInformation[] revisions = ((VersionableV2) resource).getRevisions();
+            if (revisions !=  null && revisions.length > 0) {
+                sb.append("<li class=\"haschild\">Revisions&#160;&#160;&#160;<ul>");
+                for (int i = 0; i < revisions.length; i++) {
+                    sb.append("<li class=\"haschild\">"+revisions[i].getName()+"&#160;&#160;&#160;<ul><li><a href=\"?yanel.resource.revision=" + revisions[i].getName() + "\">View</a></li><li>Show diff</li><li>Revert to</li></ul></li>");
+                }
+                sb.append("</ul></li>");
+            }
+        }
+        sb.append("</ul>");
+        sb.append("</li></ul>");
+
+        return sb.toString();
+    }
+}
