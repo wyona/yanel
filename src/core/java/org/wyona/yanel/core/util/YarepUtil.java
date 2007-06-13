c334b7271e50370bea7df480d41f6c77278985f0
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-06-13 19:39:54 +0000

    yarep util started

---
+++ src/core/java/org/wyona/yanel/core/util/YarepUtil.java
@@ -0,0 +1,40 @@
+package org.wyona.yanel.core.util;
+
+import org.apache.log4j.Category;
+
+import org.wyona.commons.io.Path;
+
+import org.wyona.yarep.core.Node;
+import org.wyona.yarep.core.NodeType;
+import org.wyona.yarep.core.Repository;
+import org.wyona.yarep.core.RepositoryException;
+
+/**
+ *
+ */
+public class YarepUtil {
+
+    private static Category log = Category.getInstance(YarepUtil.class);
+
+    /**
+     *
+     */
+    public static Node addNodes(Repository repo, String path, int nodeType) throws RepositoryException {
+        if (repo.existsNode(path)) {
+            return repo.getNode(path);
+        } else {
+            Path parentPath = new Path(path).getParent();
+            if (parentPath != null) {
+                Node parentNode = null;
+                if (repo.existsNode(parentPath.toString())) {
+                    parentNode = repo.getNode(parentPath.toString());
+                } else {
+                    parentNode = addNodes(repo, parentPath.toString(), org.wyona.yarep.core.NodeType.COLLECTION);
+                }
+                return parentNode.addNode(new Path(path).getName().toString(), nodeType);
+            } else {
+                throw new RepositoryException("Root node does not have a parent!");
+            }
+        }
+    }
+}
