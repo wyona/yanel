3fe05f5608d3bb168bd845a65c9569be81d86e3b
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-06-13 19:15:46 +0000

    addNodes method started

--- src/core/java/org/wyona/yanel/core/util/Mkdirs.java
+++ src/core/java/org/wyona/yanel/core/util/Mkdirs.java
@@ -1,6 +1,23 @@
 package org.wyona.yanel.core.util;
 
+import org.wyona.yarep.core.Node;
+import org.wyona.yarep.core.NodeType;
+import org.wyona.yarep.core.Repository;
+
+import org.apache.log4j.Category;
+
 /**
+ *
  */
 public class Mkdirs {
+
+    private static Category log = Category.getInstance(Mkdirs.class);
+
+    /**
+     *
+     */
+    public static Node addNodes(Repository repo, String path, int nodeType) {
+        log.error("DEBUG: Path: " + path);
+        return null;
+    }
 }
