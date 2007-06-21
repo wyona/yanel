443d6428028462531bf38bbda5369a1650a7b3e4
simon <simon@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-06-21 12:56:16 +0000

    adding prototype.js and progressBar.js to global htdocs

---
+++ src/webapp/htdocs/yanel-js/progressBar.js
@@ -0,0 +1,28 @@
+var i = 0;
+var flag;
+var yanelProgressBarActive;
+
+function startMoveProgressBar () {
+  yanelProgressBarActive = window.setInterval('moveProgressBar()', 1);
+}
+
+function moveProgressBar () {
+  if (i == 0 || flag == "a") {
+    i = i + 1;
+    flag = "a";
+  }
+  if (i >= 190 || flag == "d") {
+    i = i - 1;
+    flag = "d";
+  }
+  if (document.getElementById("yanelprogressbarindicator") != null) {
+    document.getElementById('yanelprogressbarindicator').style.left = i-80 + '%';
+  } else {
+    window.clearInterval(yanelProgressBarActive); 
+  }
+}
+
+function createProgressBar (target) {
+  $(target).update('<div id="yanelprogressbar"><div id="yanelprogressbarindicator">|||||||||</div></div>');
+  startMoveProgressBar();
+}
