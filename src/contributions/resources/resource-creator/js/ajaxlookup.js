453e75635e89686d2be762e15cf14e3b976bd244
simon <simon@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-06-22 14:44:22 +0000

    obsolete

--- src/contributions/resources/resource-creator/js/ajaxlookup.js
+++
@@ -1,3 +0,0 @@
-function ajaxlookup(resourcetype, lookin) {
-  new Ajax.Updater('lookup', '', { method: 'get', parameters: {lookup: 'true', 'yanel.resource.viewid': 'source', lookin: lookin, 'resource-type': resourcetype} });
-}
