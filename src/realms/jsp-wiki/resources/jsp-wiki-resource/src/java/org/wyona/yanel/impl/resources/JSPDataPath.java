3e270dfc66788d6d780e14b14d2182caa8a70f48
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-07-10 12:57:16 +0000

    JSP data path added

---
+++ src/realms/jsp-wiki/resources/jsp-wiki-resource/src/java/org/wyona/yanel/impl/resources/JSPDataPath.java
@@ -0,0 +1,25 @@
+package org.wyona.yanel.impl.resources;
+
+import org.apache.log4j.Category;
+
+/**
+ *
+ */
+public class JSPDataPath implements DataPath {
+
+    private static Category log = Category.getInstance(JSPDataPath.class);
+
+    /*
+     *
+     */
+    public String getDataPath(String path) {
+        String dataPath = null;
+        if (path.indexOf(".html") > 0) {
+            dataPath = path.substring(0, path.lastIndexOf(".html")) + ".txt";
+        } else {
+            dataPath = path + ".txt";
+        }
+        log.error("DEBUG: Path: " + path + ", Data path: " + dataPath);
+        return dataPath;
+    }
+}
