92c1762e5f63e2d887d08b0e7be4ffbb0a580556
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-07-10 12:37:38 +0000

    allow various data path implementations

---
+++ src/contributions/resources/wiki/src/java/org/wyona/yanel/impl/resources/DataPath.java
@@ -0,0 +1,12 @@
+package org.wyona.yanel.impl.resources;
+
+/**
+ *
+ */
+public interface DataPath {
+
+    /*
+     *
+     */
+    public String getDataPath(String path);
+}
---
+++ src/contributions/resources/wiki/src/java/org/wyona/yanel/impl/resources/DefaultDataPath.java
@@ -0,0 +1,14 @@
+package org.wyona.yanel.impl.resources;
+
+/**
+ *
+ */
+public class DefaultDataPath implements DataPath {
+
+    /*
+     *
+     */
+    public String getDataPath(String path) {
+        return path;
+    }
+}
