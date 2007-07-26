ffdcdbbac9c2aa54dfee46077733399b49da6152
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-07-26 21:19:48 +0000

    timeline resource started

---
+++ src/contributions/resources/timeline/src/java/edu/mit/simile/yanel/impl/resources/timeline/TimelineResource.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2007 Wyona
+ */
+
+package edu.mit.simile.yanel.impl.resources.timeline;
+
+import org.wyona.yanel.core.Resource;
+
+/**
+ *
+ */
+public class TimelineResource extends Resource {
+
+    /**
+     *
+     */
+    public TimelineResource() {
+    }
+}
