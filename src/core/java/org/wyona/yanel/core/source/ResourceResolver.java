6ec5e31eddb3a3ea1ba084ced7d1501130b9013d
jon <jon@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-11-15 16:22:49 +0000

    changed view.isResponse() to !view.isResponse().

--- src/core/java/org/wyona/yanel/core/source/ResourceResolver.java
+++ src/core/java/org/wyona/yanel/core/source/ResourceResolver.java
@@ -67,7 +67,7 @@ public class ResourceResolver implements URIResolver {
                 return new StreamSource(view.getInputStream());
             } else if (ResourceAttributeHelper.hasAttributeImplemented(targetResource, "Viewable", "2")) {
                 View view = ((ViewableV2) targetResource).getView(null);
-                if (view.isResponse()) {
+                if (!view.isResponse()) {
                     throw new Exception("Reading from the response has not been implemented yet!");
                 } else {
                     return new StreamSource(view.getInputStream());
