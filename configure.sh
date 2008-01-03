6265130f6c42ec974c06e77d7da4f8d3729b6e17
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-01-03 10:43:11 +0000

    JAVA_HOME help note improved

--- configure.sh
+++ configure.sh
@@ -9,7 +9,11 @@ JAVA_HOME="$JAVA_HOME"
 if [ "$JAVA_HOME" = "" ];then
   echo "ERROR: No JAVA_HOME set!"
   echo "       Have you installed JDK (Java Development Kit)? If so, then set JAVA_HOME ..."
-  echo "       MacOS X : setenv JAVA_HOME /System/Library/Frameworks/JavaVM.framework/Home"
+  echo ""
+  echo "       MacOS X : Depending on the shell you're using either use"
+  echo "                 setenv JAVA_HOME /System/Library/Frameworks/JavaVM.framework/Home"
+  echo "                 or"
+  echo "                 export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home"
   echo "       Linux   : export JAVA_HOME=/usr/local/j2sdk-..."
   echo "       Windows : Click Start, click 'My Computer', right click on window, select 'Properties', click the 'Advanced' tab, click 'Environment Variables'"
   exit 1
