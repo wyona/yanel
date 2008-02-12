c6a8cb3f5a3c0d86de20ac423483205a77625247
simon <simon@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-02-12 08:22:07 +0000

    fixes bug: http://bugzilla.wyona.com/cgi-bin/bugzilla/show_bug.cgi?id=5964. thanks to josias!

--- src/impl/java/org/wyona/yanel/impl/resources/usecase/UsecaseResource.java
+++ src/impl/java/org/wyona/yanel/impl/resources/usecase/UsecaseResource.java
@@ -135,7 +135,7 @@ public class UsecaseResource extends BasicXMLResource {
             //jellyContext.setVariable("request", request);
 
             ByteArrayOutputStream jellyResultStream = new ByteArrayOutputStream();
-            XMLOutput jellyOutput = XMLOutput.createXMLOutput(jellyResultStream);
+            XMLOutput jellyOutput = XMLOutput.createXMLOutput(jellyResultStream, true);
             
             //String viewTemplate = view.getTemplate();
             jellyContext.runScript(new InputSource(repo.getNode(viewTemplate).getInputStream()), jellyOutput);
