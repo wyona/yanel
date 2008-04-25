da7dff7a8881837695de822758f369fa33ca5f38
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-04-25 07:31:43 +0000

    catch exception and output creation and last access time

--- src/contributions/resources/sessionmanager/src/java/org/wyona/yanel/impl/resources/sessionmanager/SessionManagerResource.java
+++ src/contributions/resources/sessionmanager/src/java/org/wyona/yanel/impl/resources/sessionmanager/SessionManagerResource.java
@@ -9,6 +9,7 @@ import org.wyona.yanel.servlet.IdentityMap;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Date;
 
 import org.apache.log4j.Logger;
 
@@ -30,15 +31,21 @@ public class SessionManagerResource extends BasicXMLResource {
         sb.append("<session-manager xmlns=\"http://www.wyona.org/yanel/1.0\">");
         javax.servlet.http.HttpSession[] activeSessions = org.wyona.yanel.servlet.SessionCounter.getActiveSessions();
         sb.append("<number-of-sessions>" + activeSessions.length + "</number-of-sessions>");
+
+        java.text.DateFormat dateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss:SSZ");
         for (int i = 0; i < activeSessions.length; i++) {
-            sb.append("<session id=\"" + activeSessions[i].getId() + "\">");
-            IdentityMap identityMap = (IdentityMap) activeSessions[i].getAttribute(org.wyona.yanel.servlet.YanelServlet.IDENTITY_MAP_KEY);
-            if (identityMap != null) {
-                sb.append("<identities>" + identityMap.toString() + "</identities>");
-            } else {
-                sb.append("<no-identity-yet/>");
+            try {
+                sb.append("<session id=\"" + activeSessions[i].getId() + "\" creation-time=\"" + dateFormat.format(new Date(activeSessions[i].getCreationTime())) + "\" last-accessed-time=\"" + dateFormat.format(new Date(activeSessions[i].getLastAccessedTime())) + "\">");
+                IdentityMap identityMap = (IdentityMap) activeSessions[i].getAttribute(org.wyona.yanel.servlet.YanelServlet.IDENTITY_MAP_KEY);
+                if (identityMap != null) {
+                    sb.append("<identities>" + identityMap.toString() + "</identities>");
+                } else {
+                    sb.append("<no-identity-yet/>");
+                }
+                sb.append("</session>");
+            } catch (Exception e) {
+                sb.append("<exception session-id=\"" + activeSessions[i].getId() + "\">" + e.getMessage() + "</exception>");
             }
-            sb.append("</session>");
         }
         sb.append("</session-manager>");
         return new ByteArrayInputStream(sb.toString().getBytes());
