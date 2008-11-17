73e3b340e7654bda9497717685262bad32f48e48
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-11-17 11:10:36 +0000

    setting JAVA_HOME instruction improved

--- src/binary-dist/start.sh
+++ src/binary-dist/start.sh
@@ -7,8 +7,11 @@ JAVA_HOME="$JAVA_HOME"
 if [ "$JAVA_HOME" = "" ];then
   echo "ERROR: No JAVA_HOME set!"
   echo "       Have you installed JDK (Java Development Kit)? If so, then set JAVA_HOME ..."
-  echo "       MacOS X : setenv JAVA_HOME /System/Library/Frameworks/JavaVM.framework/Home"
-  echo "       Linux   : export JAVA_HOME=/usr/local/j2sdk-..."
+  echo "       MacOS X : Depending on the shell you're using either use"
+  echo "                 setenv JAVA_HOME /System/Library/Frameworks/JavaVM.framework/Home"
+  echo "                 or"
+  echo "                 export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home"
+  echo "       Linux   : export JAVA_HOME=/usr/local/jdk-1.5.0 (whereas '/usr/local/jdk-1.5.0' is just an example path)"
   exit 1
 fi
 
