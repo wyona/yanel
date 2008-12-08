150c2c533c5f1de749914c264a138847f0a2f9d7
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-12-08 15:16:48 +0000

    obsolete imports and else statements removed

--- src/core/java/org/wyona/yanel/core/util/YarepUtil.java
+++ src/core/java/org/wyona/yanel/core/util/YarepUtil.java
@@ -1,11 +1,8 @@
 package org.wyona.yanel.core.util;
 
-import org.apache.log4j.Category;
-
 import org.wyona.commons.io.Path;
 
 import org.wyona.yarep.core.Node;
-import org.wyona.yarep.core.NodeType;
 import org.wyona.yarep.core.Repository;
 import org.wyona.yarep.core.RepositoryException;
 
@@ -13,28 +10,23 @@ import org.wyona.yarep.core.RepositoryException;
  * @deprecated Use org.wyona.yarep.util.YarepUtil
  */
 public class YarepUtil {
-
-    private static Category log = Category.getInstance(YarepUtil.class);
-
     /**
      * Creates the node named by this abstract pathname, including any necessary but nonexistent parent nodes (similar to java.io.File.mkdirs()).
      */
     public static Node addNodes(Repository repo, String path, int nodeType) throws RepositoryException {
         if (repo.existsNode(path)) {
             return repo.getNode(path);
-        } else {
-            Path parentPath = new Path(path).getParent();
-            if (parentPath != null) {
-                Node parentNode = null;
-                if (repo.existsNode(parentPath.toString())) {
-                    parentNode = repo.getNode(parentPath.toString());
-                } else {
-                    parentNode = addNodes(repo, parentPath.toString(), org.wyona.yarep.core.NodeType.COLLECTION);
-                }
-                return parentNode.addNode(new Path(path).getName().toString(), nodeType);
+        } 
+        Path parentPath = new Path(path).getParent();
+        if (parentPath != null) {
+            Node parentNode = null;
+            if (repo.existsNode(parentPath.toString())) {
+                parentNode = repo.getNode(parentPath.toString());
             } else {
-                throw new RepositoryException("Root node does not have a parent!");
+                parentNode = addNodes(repo, parentPath.toString(), org.wyona.yarep.core.NodeType.COLLECTION);
             }
+            return parentNode.addNode(new Path(path).getName().toString(), nodeType);
         }
+        throw new RepositoryException("Root node does not have a parent!");
     }
 }
