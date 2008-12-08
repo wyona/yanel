2125c8eaa26b07a7bce4ad1784838169808efc8b
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-12-08 15:21:59 +0000

    obsolete import removed

--- src/core/java/org/wyona/yanel/core/workflow/WorkflowBuilder.java
+++ src/core/java/org/wyona/yanel/core/workflow/WorkflowBuilder.java
@@ -19,7 +19,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
