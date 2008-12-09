5b8a66620c077a564292a1f1ec53b68af3ef9412
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-12-09 10:07:05 +0000

    obsolete Category and else removed

--- src/core/java/org/wyona/yanel/core/attributes/translatable/SuffixTranslationManager.java
+++ src/core/java/org/wyona/yanel/core/attributes/translatable/SuffixTranslationManager.java
@@ -16,16 +16,12 @@
 
 package org.wyona.yanel.core.attributes.translatable;
 
-import org.apache.log4j.Category;
-
 /**
  * This translation manager works with a path format where the language
  * is a suffix: /foo/bar_en.html
  */
 public class SuffixTranslationManager extends AbstractPathTranslationManager {
 
-    private static Category log = Category.getInstance(SuffixTranslationManager.class);
-    
     /**
      * Empty constructor. Don't forget to call the init() method.
      */
@@ -50,9 +46,8 @@ public class SuffixTranslationManager extends AbstractPathTranslationManager {
         }
         if (suffix != null && suffix.length() == 2) {
             return suffix;
-        } else {
-            return null;
         }
+        return null;
     }
     
     /**
@@ -77,9 +72,8 @@ public class SuffixTranslationManager extends AbstractPathTranslationManager {
         }
         if (basePath != null && basePath.length() > 0) {
             return basePath + "_" + language + "." + extension;
-        } else {
-            return null;
         }
+        return null;
     }
     
 }
