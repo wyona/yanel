889d67988a34534c106a01bfca6ef25a70553950
simon <simon@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-12-22 01:03:50 +0000

    simplify/refactor Yanel configure scripts. see bug: http://bugzilla.wyona.com/cgi-bin/bugzilla/show_bug.cgi?id=6808 - thanks to guillaume.

--- yanel.bat
+++ yanel.bat
@@ -1,7 +1,5 @@
 @echo off
 
-echo "Start/Stop Yanel ..."
-
 :: Check if JAVA_HOME is set or goto end
 if not "%JAVA_HOME%" == "" goto gotJavaHome
 echo You must set JAVA_HOME to point at your Java Development Kit installation
@@ -14,24 +12,37 @@ set ANT_HOME=tools\apache-ant-1.6.5
 
 call %ANT_HOME%\bin\ant -version
 
-::----- Start/Stop Yanel
+rem ----- Yanel subcommands:
 if "%1" == "start"; goto start
 if "%1" == "stop"; goto stop
 if "%1" == "start-jetty"; goto startJetty
+if "%1" == "configure"; goto configure
 goto cmdl
 
 :start
-  %ANT_HOME%\bin\ant -f src/build/build.xml start-tomcat
+  echo "INFO: Starting Yanel..."
+  call %ANT_HOME%\bin\ant -f src/build/build.xml start-tomcat
+  goto restoreAntHome
 :stop
-  %ANT_HOME%\bin\ant -f src/build/build.xml stop-tomcat
+  echo "INFO: Stopping Yanel..."
+  call %ANT_HOME%\bin\ant -f src/build/build.xml stop-tomcat
+  goto restoreAntHome
 :startJetty
+  echo "INFO: Starting Yanel on Jetty..."
   path | find /i "maven-2.0.4\bin" > nul
   if errorlevel 1 goto MavenNotFound 
   shift
   shift
-  mvn jetty:run-war -f  src/build/pom-webapp.xml %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+  call mvn jetty:run-war -f  src/build/pom-webapp.xml %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+  goto restoreAntHome
+:configure
+  echo "INFO: Configuring Yanel..."
+  rem %ANT_HOME%\bin\ant -f src\build\build.xml -Djava.endorsed.dirs=lib\endorsed -logger org.apache.tools.ant.NoBannerLogger -emacs config
+  call %ANT_HOME%\bin\ant -f src/build/build.xml config
+  goto restoreAntHome
 :cmdl
-  %ANT_HOME%\bin\ant -f src/build/build.xml run-yanel-cmdl -Dyanel.path=""
+  call %ANT_HOME%\bin\ant -f src/build/build.xml run-yanel-cmdl -Dyanel.path=""
+  goto restoreAntHome
 
 
 :MavenNotFound
@@ -41,6 +52,7 @@ echo        If so, then check your PATH and try again
 echo        or install Maven 2.0.4 from http://maven.apache.org
 
 
+:restoreAntHome
 rem ----- Restore ANT_HOME
 set ANT_HOME=%ORIGINAL_ANT_HOME%
 set ORIGINAL_ANT_HOME=
--- yanel.sh
+++ yanel.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-echo "Start/Stop Yanel ..."
-
 # ----- Check for JAVA_HOME
 JAVA_HOME="$JAVA_HOME"
 if [ "$JAVA_HOME" = "" ];then
   echo "ERROR: No JAVA_HOME set!"
   echo "       Have you installed JDK (Java Development Kit)? If so, then set JAVA_HOME ..."
+  echo ""
   echo "       MacOS X : Depending on the shell you're using either use"
   echo "                 setenv JAVA_HOME /System/Library/Frameworks/JavaVM.framework/Home"
   echo "                 or"
   echo "                 export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home"
   echo "       Linux   : export JAVA_HOME=/usr/local/jdk-1.5.0 (whereas '/usr/local/jdk-1.5.0' is just an example path)"
-  echo "       Windows : Click Start ..."
+  echo "       Windows : Click Start, click 'My Computer', right click on window, select 'Properties', click the 'Advanced' tab, click 'Environment Variables'"
   exit 1
 fi
+
+# ----- Check Java version
+# TODO: ....
 
 # ----- Set Environment Variables
 unset ANT_HOME
@@ -26,14 +28,17 @@ unset CATALINA_HOME
 PATH=$PWD/tools/maven-2.0.4/bin:$ANT_HOME/bin:$PATH
 #echo $PATH
 
-# ----- Start/Stop Yanel
+# ----- Yanel subcommands:
 #mvn --version
 ant -version
 if [ "$1" = "start" ]; then
+  echo "INFO: Starting Yanel..."
   ant -f src/build/build.xml start-tomcat
 elif [ "$1" = "stop" ]; then
+  echo "INFO: Stopping Yanel..."
   ant -f src/build/build.xml stop-tomcat
 elif [ "$1" = "start-jetty" ]; then
+  echo "INFO: Starting Yanel on Jetty..."
   MAVEN=`which mvn`
   if [ -z "$MAVEN" ]; then
     echo "ERROR: No \"mvn\" command available!"
@@ -43,6 +48,9 @@ elif [ "$1" = "start-jetty" ]; then
     ant -f src/build/build.xml war
     mvn jetty:run-war -f  src/build/pom-jetty.xml $@
   fi  
+elif [ "$1" = "configure" ]; then
+  echo "INFO: Configuring Yanel..."
+  ant -f src/build/build.xml config
 else
   ant -f src/build/build.xml run-yanel-cmdl -Dyanel.path=$1
 fi
