274cfa9d26f607c227bb950d797b8c5559479964
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2009-01-14 15:54:45 +0000

    import RevisionInformationMenuItem

--- src/realms/yanel-website/src/java/org/wyona/yanel/website/menu/impl/YanelWebsiteMenu.java
+++ src/realms/yanel-website/src/java/org/wyona/yanel/website/menu/impl/YanelWebsiteMenu.java
@@ -8,6 +8,7 @@ import org.wyona.yanel.core.attributes.versionable.RevisionInformation;
 import org.wyona.yanel.core.map.Map;
 import org.wyona.yanel.core.util.ResourceAttributeHelper;
 import org.wyona.yanel.servlet.menu.Menu;
+import org.wyona.yanel.servlet.menu.impl.RevisionInformationMenuItem;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
