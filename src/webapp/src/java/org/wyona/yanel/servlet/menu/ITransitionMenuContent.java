32fbfc9d04fe84e81ebeaf3aa6e2fd590d37ddcb
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2009-01-20 13:51:08 +0000

    javadoc improved

--- src/webapp/src/java/org/wyona/yanel/servlet/menu/ITransitionMenuContent.java
+++ src/webapp/src/java/org/wyona/yanel/servlet/menu/ITransitionMenuContent.java
@@ -6,17 +6,17 @@ package org.wyona.yanel.servlet.menu;
 import org.wyona.yanel.core.workflow.Transition;
 
 /**
- * @author gary
  *
  */
 public interface ITransitionMenuContent {
 
     /**
-     * Returns a LI element, containing either a link to activate the desired action,
-     * or plain text if it is not allowed to take the action based on the revision state.
-     * @param t
-     * @return
+     * Returns a <li>...</li> element representing an individual transition,
+     * containing either a link to execute the corresponding transition or plain
+     * text if it is not allowed to take the action based on the revision state.
+     * @param transition the transition to be represented.
+     * @return List element <li>...</li>
      */
-    public abstract String getTransitionElement(final Transition t);
+    public abstract String getTransitionElement(final Transition transition);
 
 }
