706496b2717645014bef56ebdb9e643bf9d7a601
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2009-01-20 13:40:42 +0000

    thanks to Gary javadoc added

--- src/webapp/src/java/org/wyona/yanel/servlet/menu/AnchorElement.java
+++ src/webapp/src/java/org/wyona/yanel/servlet/menu/AnchorElement.java
@@ -4,19 +4,26 @@
 package org.wyona.yanel.servlet.menu;
 
 /**
- * @author gary
- *
+ * A very simple representation of an html <a> element.
  */
 public class AnchorElement {
     
     private String label;
     private String url;
     
+    /**
+     * ctor.
+     * @param label the label of the anchor, i.e. the text displayed to the user in a browser
+     * @param url the target url, i.e. the href part of the anchor
+     */
     public AnchorElement(final String label, final String url) {
         this.label = label;
         this.url = url;
     }
     
+    /**
+     * Returns a properly formed anchor element as a string 
+     */
     public String toString() {
         return "<a href='" + url + "'>" + label + "</a>";
     }
