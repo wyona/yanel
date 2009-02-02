bb188427f34b81043321539f9359be81d9f493df
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2009-02-02 09:18:12 +0000

    about refactored

---
+++ src/webapp/src/java/org/wyona/yanel/servlet/About.java
@@ -0,0 +1,18 @@
+package org.wyona.yanel.servlet;
+
+/**
+ * Yanel about statement
+ */
+public class About {
+
+    /**
+     * Get XHTML of about statement
+     */
+    public static String toHTML(String version, String revision) {
+        StringBuilder sb = new StringBuilder("<html>");
+        sb.append("<head><title>About Yanel</title></head>");
+        sb.append("<body><h1>About Yanel</h1><p>Version " + version + "-r" + revision + "</p><p>Copyright &#169; 2005 - 2009 Wyona. All rights reserved.</p></body>");
+        sb.append("</html>");
+        return sb.toString();
+    }
+}
