e6cc5591f563269ad96a2b2aa306910c9b7879a3
simon <simon@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2009-04-14 15:31:03 +0000

    lookup integrated

--- src/resources/tinymce/htdocs/js/ajaxlookup.js
+++
@@ -1,3 +0,0 @@
-function ajaxlookup(lookin) {
-  new Ajax.Updater('lookup', '', { method: 'get', onLoading: createProgressBar('lookupfiles'), onComplete: sorttable.reinit, parameters: {lookup: 'true', 'yanel.resource.viewid': 'source', lookin: lookin} });
-}
