3be88bc528cccdd73d1da32b76560b7d290755a0
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2009-05-21 11:50:08 +0000

    refactor Neutron classes into separate library

---
+++ src/neutron/java/org/wyona/neutron/XMLExceptionV1.java
@@ -0,0 +1,39 @@
+package org.wyona.neutron;
+
+/**
+ * Utility class in order to generate XML based on specification: http://neutron.wyona.org/draft-neutron-protocol-v0.html
+ */
+public class XMLExceptionV1 {
+
+    // http://neutron.wyona.org/draft-neutron-protocol-v0.html#rfc.section.8
+    public static int AUTHORIZATION = 0;
+    public static int AUTHENTICATION = 1;
+    public static int CHECKOUT = 2;
+    public static int CHECKIN = 3;
+    public static int DATA_NOT_WELL_FORMED = 4;
+
+    /**
+     *
+     */
+    public String getDefaultException(int type, String message) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<?xml version=\"1.0\"?>");
+        sb.append("<exception xmlns=\"http://www.wyona.org/neutron/1.0\" type=\"" + getTypeAsString(type) + "\">");
+        sb.append("<message>" + message + "</message>");
+        sb.append("</exception>");
+        return sb.toString();
+    }
+
+    /**
+     *
+     */
+    public static String getTypeAsString(int type) {
+        if (type == AUTHORIZATION) {
+            return "authorization";
+        } else if (type == DATA_NOT_WELL_FORMED) {
+            return "data-not-well-formed";
+        } else {
+            return "unknown";
+        }
+    }
+}
