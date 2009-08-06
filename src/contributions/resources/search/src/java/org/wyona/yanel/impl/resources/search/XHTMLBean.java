13a42a5767f22092dd599e058bdfb883261cb554
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2009-08-06 20:35:09 +0000

    utility classes added

---
+++ src/contributions/resources/search/src/java/org/wyona/yanel/impl/resources/search/XHTMLBean.java
@@ -0,0 +1,26 @@
+/**
+ * Licensed to Wyona
+ */
+package org.wyona.yanel.impl.resources.search;
+
+/**
+ *
+ */
+public class XHTMLBean {
+
+    private String title;
+
+    /**
+     *
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * /html/head/title
+     */
+    public String getTitle() {
+        return title;
+    }
+}
