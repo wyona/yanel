3ee72530f80085d8bbf6817977b3471e97d85b06
guillaume <guillaume@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2009-08-27 10:33:22 +0000

    XInclude inclusions with relative URLs should now also work (untested),  similarly to XSLT includes and imports as done in revision 44431.
    Issue: 6382

--- src/core/java/org/wyona/yanel/core/transformation/XIncludeTransformer.java
+++ src/core/java/org/wyona/yanel/core/transformation/XIncludeTransformer.java
@@ -3,7 +3,7 @@ package org.wyona.yanel.core.transformation;
 import javax.xml.transform.URIResolver;
 import javax.xml.transform.sax.SAXSource;
 
-import org.apache.log4j.Category;
+import org.apache.log4j.Logger;
 import org.apache.xml.resolver.tools.CatalogResolver;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -11,15 +11,15 @@ import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
 
 /**
- * Transformer to include xml into a sax stream.
+ * Transformer to include XML into a SAX stream.
  * It will resolve <xi:include href="bla.xml"/>. 
- * The namespace uri is http://www.w3.org/2001/XInclude
- * This is not a complete implementation of the xinclude specification.
+ * The namespace-URI is http://www.w3.org/2001/XInclude
+ * This is not a complete implementation of the XInclude specification.
  * Also see http://www.w3.org/TR/xinclude/
  */
 public class XIncludeTransformer extends AbstractTransformer {
 
-    private static Category log = Category.getInstance(XIncludeTransformer.class);
+    private static final Logger log = Logger.getLogger(XIncludeTransformer.class);
 
     public static final String NS_URI = "http://www.w3.org/2001/XInclude";
     
@@ -36,18 +36,21 @@ public class XIncludeTransformer extends AbstractTransformer {
         this.resolver = resolver;
     }
 
+    @Override
     public void startDocument() throws SAXException {
         if (!this.ignoreDocumentEvent) {
             super.startDocument();
         }
     }
-    
+
+    @Override
     public void endDocument() throws SAXException {
         if (!this.ignoreDocumentEvent) {
             super.endDocument();
         }
     }
-    
+
+    @Override
     public void startElement(String namespaceURI, String localName, String qName, Attributes attrs) throws SAXException {
         
         if (namespaceURI.equals(NS_URI) && localName.equals("include")) {
@@ -61,7 +64,7 @@ public class XIncludeTransformer extends AbstractTransformer {
             
             this.ignoreDocumentEvent = true;
             try {
-                xmlReader.parse(SAXSource.sourceToInputSource(resolver.resolve(href, null)));
+                xmlReader.parse(SAXSource.sourceToInputSource(resolver.resolve(href, href)));
             } catch (Exception e) {
                 log.error("XInclude error for href: " + href + ":  " + e, e);
                 throw new SAXException(e);
@@ -76,6 +79,7 @@ public class XIncludeTransformer extends AbstractTransformer {
         }
     }
 
+    @Override
     public void endElement(String namespaceURI, String localName, String qName) throws SAXException {
         if (!this.insideIncludeElement) {
             super.endElement(namespaceURI, localName, qName);
