d84ca72410ab259e893230a467a6b1d33509fcdc
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2009-09-28 07:22:28 +0000

    setting JAVA_HOME made user friendlier on Mac OS X

--- yanel.sh
+++ yanel.sh
@@ -3,19 +3,36 @@
 SCRIPT_DIR=$PWD
 #SCRIPT_DIR=$PWD/`dirname $0`
 
+JAVA_HOME_MACOSX=/System/Library/Frameworks/JavaVM.framework/Home
+
 # ----- Check for JAVA_HOME
 JAVA_HOME="$JAVA_HOME"
 if [ "$JAVA_HOME" = "" ];then
   echo "ERROR: No JAVA_HOME set!"
   echo "       Have you installed JDK (Java Development Kit)? If so, then set JAVA_HOME ..."
   echo ""
-  echo "       MacOS X : Depending on the shell you're using either use"
-  echo "                 setenv JAVA_HOME /System/Library/Frameworks/JavaVM.framework/Home"
-  echo "                 or"
-  echo "                 export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home"
-  echo "       Linux   : export JAVA_HOME=/usr/local/jdk-1.5.0 (whereas '/usr/local/jdk-1.5.0' is just an example path)"
-  echo "       Windows : Click Start, click 'My Computer', right click on window, select 'Properties', click the 'Advanced' tab, click 'Environment Variables'"
-  exit 1
+  echo "       Mac OS X : Depending on the shell you're using either use"
+  echo "                  export JAVA_HOME=$JAVA_HOME_MACOSX"
+  echo "                  or"
+  echo "                  setenv JAVA_HOME $JAVA_HOME_MACOSX"
+  echo "       Linux    : export JAVA_HOME=/usr/local/jdk-1.5.0 (whereas '/usr/local/jdk-1.5.0' is just an example path)"
+  echo "       Windows  : Click Start, click 'My Computer', right click on window, select 'Properties', click the 'Advanced' tab, click 'Environment Variables'"
+  echo ""
+  if [ -d $JAVA_HOME_MACOSX ]; then
+      echo "INFO: You seem to use Mac OS X as operating system. Do you want to set '$JAVA_HOME_MACOSX' as JAVA_HOME? (YES/no)"
+      read ANSWER
+      if [ "$ANSWER" = "no" ]; then
+          echo "WARNING: JAVA_HOME has not been set. Please make sure to set it manually and then re-run this script."
+          exit 1
+      else
+          export JAVA_HOME=$JAVA_HOME_MACOSX
+          echo "INFO: JAVA_HOME has been set to '$JAVA_HOME_MACOSX' while processing this shell script."
+          echo ""
+          sleep 3
+      fi
+  else
+      exit 1
+  fi
 fi
 
 # ----- Check Java version
