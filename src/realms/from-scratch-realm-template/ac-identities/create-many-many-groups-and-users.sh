2fd80934b98845008cbaf86e87e1c3de36a33ed4
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2009-12-11 15:04:42 +0000

    create 1200 users changed

--- src/realms/from-scratch-realm-template/ac-identities/create-many-many-groups-and-users.sh
+++ src/realms/from-scratch-realm-template/ac-identities/create-many-many-groups-and-users.sh
@@ -3,7 +3,7 @@
 echo "Create many, many, many groups and users ..."
 
 count=0
-while [ $count -lt 5 ]
+while [ $count -lt 1200 ]
 do
     echo "<?xml version=\"1.0\"?>\n<group id=\"group$count\" xmlns=\"http://www.wyona.org/security/1.0\">\n<name>Example Group $count</name>\n</group>" > groups/group$count.xml
     count=`expr $count + 1`
