324cf55161a401f1ad3fb733ed518e61c3505b09
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2010-01-07 11:14:08 +0000

    year fixed

--- src/webapp/src/java/org/wyona/yanel/servlet/About.java
+++ src/webapp/src/java/org/wyona/yanel/servlet/About.java
@@ -11,7 +11,7 @@ public class About {
     public static String toHTML(String version, String revision) {
         StringBuilder sb = new StringBuilder("<html>");
         sb.append("<head><title>About Yanel</title></head>");
-        sb.append("<body><h1>About Yanel</h1><p>Version " + version + "-r" + revision + "</p><p>Copyright &#169; 2005 - 2009 Wyona. All rights reserved.</p></body>");
+        sb.append("<body><h1>About Yanel</h1><p>Version " + version + "-r" + revision + "</p><p>Copyright &#169; 2005 - 2010 Wyona. All rights reserved.</p></body>");
         sb.append("</html>");
         return sb.toString();
     }
