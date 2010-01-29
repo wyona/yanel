ddcb18b7fc0d964daf9ff1c910e1dbada3becf6b
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2010-01-29 14:25:56 +0000

    new menu version added

---
+++ src/webapp/src/java/org/wyona/yanel/servlet/menu/impl/DefaultMenuV2.java
@@ -0,0 +1,61 @@
+package org.wyona.yanel.servlet.menu.impl;
+
+import org.wyona.yanel.core.Resource;
+import org.wyona.yanel.core.api.attributes.VersionableV2;
+import org.wyona.yanel.core.api.attributes.WorkflowableV1;
+import org.wyona.yanel.core.attributes.versionable.RevisionInformation;
+import org.wyona.yanel.core.map.Map;
+import org.wyona.yanel.core.util.ResourceAttributeHelper;
+import org.wyona.yanel.core.workflow.Transition;
+import org.wyona.yanel.core.workflow.Workflow;
+import org.wyona.yanel.core.workflow.WorkflowHelper;
+
+import org.wyona.yanel.servlet.menu.Menu;
+import org.wyona.yanel.servlet.menu.impl.RevisionInformationMenuItem;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This menu implements the new tinymce suffix (In order to stay backwards compatible this new version was created)
+ */
+public class DefaultMenuV2 extends DefaultMenu {
+
+    private static Logger log = Logger.getLogger(DefaultMenuV2.class);
+
+    /**
+     * Implements the new tinymce suffix
+     */
+    public String getEditMenu(Resource resource) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<ul><li>");
+        sb.append("<div id=\"yaneltoolbar_menutitle\">Edit</div>");
+        sb.append("<ul>");
+
+        String backToRealm = org.wyona.yanel.core.util.PathUtil.backToRealm(resource.getPath());
+        sb.append("<li class=\"haschild\">Open with&#160;&#160;&#160;");
+        sb.append("<ul><li>Source editor</li>");
+        sb.append("<li class=\"haschild\">WYSIWYG editor&#160;&#160;&#160;");
+        sb.append("<ul>");
+        if (ResourceAttributeHelper.hasAttributeImplemented(resource, "Modifiable", "2")) {
+            sb.append("<li><a href=\"" + backToRealm + "usecases/xinha.html?edit-path=" + resource.getPath() + "\">Edit page with Xinha&#160;&#160;&#160;</a></li>");
+            sb.append("<li><a href=\"" + backToRealm + resource.getPath().substring(1) + ".tinymce-edit.html\">Edit page with tinyMCE&#160;&#160;&#160;</a></li>");
+        } else {
+            sb.append("<li><a>Edit page with Xinha&#160;&#160;&#160;</a></li>");
+            sb.append("<li>Edit page with tinyMCE&#160;&#160;&#160;</li>");
+        }
+        sb.append("<li><a href=\"http://www.yulup.org\">Edit page with Yulup&#160;&#160;&#160;</a></li>");
+        sb.append("</ul>");
+        sb.append("</li>");
+        sb.append("</ul>");
+        sb.append("</li>");
+
+        sb.append("</ul>");
+        sb.append("</li></ul>");
+
+        return sb.toString();
+    }
+}
