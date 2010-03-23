c64b05f10367b69de78e1c5050d9818e29788d05
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2010-03-23 11:49:57 +0000

    also create sample users

--- src/realms/from-scratch-realm-template/ac-identities/create-many-many-groups-and-users.sh
+++ src/realms/from-scratch-realm-template/ac-identities/create-many-many-groups-and-users.sh
@@ -3,8 +3,15 @@
 echo "Create many, many, many groups and users ..."
 
 count=0
-while [ $count -lt 1200 ]
+while [ $count -lt 1000 ]
 do
     echo "<?xml version=\"1.0\"?>\n<group id=\"group$count\" xmlns=\"http://www.wyona.org/security/1.0\">\n<name>Example Group $count</name>\n</group>" > groups/group$count.xml
+    count=`expr $count + 1`
+done
+
+count=0
+while [ $count -lt 1000 ]
+do
+    echo "<?xml version=\"1.0\"?>\n<user id=\"user$count\" xmlns=\"http://www.wyona.org/security/1.0\">\n<name>Example User $count</name>\n<email>user$count@yanel.org</email>\n<language>en</language>\n<password>5ebe2294ecd0e0f08eab7690d2a6ee69</password>\n<foo:custom xmlns:foo=\"http://www.foo.bar/security/1.0\">\n<foo:role>admin</foo:role>\n</foo:custom>\n</user>" > users/user$count.xml
     count=`expr $count + 1`
 done
