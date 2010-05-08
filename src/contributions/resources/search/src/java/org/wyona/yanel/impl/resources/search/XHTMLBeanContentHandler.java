9cad0b68c539e3248f84a84973c2a5ce5bbe42df
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2010-05-08 11:38:17 +0000

    debug statements commented and formatting

--- src/contributions/resources/search/src/java/org/wyona/yanel/impl/resources/search/XHTMLBeanContentHandler.java
+++ src/contributions/resources/search/src/java/org/wyona/yanel/impl/resources/search/XHTMLBeanContentHandler.java
@@ -29,42 +29,47 @@ public class XHTMLBeanContentHandler implements ContentHandler {
         this.xhtmlBean = xhtmlBean;  
     }
 
-   public void setDocumentLocator (Locator locator) { 
+    public void setDocumentLocator (Locator locator) { 
 	  log.warn("Hello from setDocumentLocator()!"); 
-	}
-
-    public void startDocument ()
-	throws SAXException  { 
-	  log.warn("Hello from startDocument()!"); 
-	}
-
-    public void endDocument()
-	throws SAXException {  
-	  log.warn("Hello from endDocument()!"); 
 	}
 
     /**
      *
      */
+    public void startDocument () throws SAXException  { 
+        //log.warn("DEBUG: Hello from startDocument()!"); 
+    }
+
+    public void endDocument() throws SAXException {  
+        //log.warn("DEBUG: Hello from endDocument()!"); 
+    }
+
+    /**
+     *
+     */
     public void startPrefixMapping (String prefix, String uri) throws SAXException {  
-        log.warn("Prefix: " + prefix);
-        log.warn("uri: " + uri);
+/*
+        log.warn("DEBUG: Prefix: " + prefix);
+        log.warn("DEBUG: uri: " + uri);
+*/
     }
 
     /**
      *
      */
     public void endPrefixMapping (String prefix) throws SAXException  {  
-        log.warn("Prefix: " + prefix);
+        //log.warn("DEBUG: Prefix: " + prefix);
     }
 
     /**
      *
      */
     public void startElement (String uri, String localName, String qName, Attributes atts) throws SAXException {  
-        log.warn("Local name: " + localName);
-        log.warn("qName: " + qName);
-        log.warn("uri: " + uri);
+/*
+        log.warn("DEBUG: Local name: " + localName);
+        log.warn("DEBUG: qName: " + qName);
+        log.warn("DEBUG: uri: " + uri);
+*/
         if (localName.equals("title")) {
             insideTitle = true;
         }
@@ -80,7 +85,7 @@ public class XHTMLBeanContentHandler implements ContentHandler {
      *
      */
     public void endElement (String uri, String localName, String qName) throws SAXException {  
-        log.warn("Local name: " + localName); 
+        //log.warn("DEBUG: Local name: " + localName); 
         if (localName.equals("title")) {
             insideTitle = false;
         }
@@ -98,19 +103,19 @@ public class XHTMLBeanContentHandler implements ContentHandler {
     public void characters (char ch[], int start, int length) throws SAXException {  
         if (insideTitle) {
             xhtmlBean.setTitle(new String(ch));
-            log.warn("DEBUG: Title: " + new String(ch));
+            //log.warn("DEBUG: Title: " + new String(ch));
         }
         if (insideMeta) {
-            log.warn("DEBUG: Meta: " + new String(ch));
+            //log.warn("DEBUG: Meta: " + new String(ch));
         }
         if (insidePara) {
-            log.warn("DEBUG: Paragraph: " + new String(ch));
+            //log.warn("DEBUG: Paragraph: " + new String(ch));
         }
     }
 
     public void ignorableWhitespace (char ch[], int start, int length)
 	throws SAXException {  
-	  log.warn("Hello from ignorableWhitespace()!"); 
+	  //log.warn("DEBUG: Hello from ignorableWhitespace()!"); 
 	}
 
     public void processingInstruction (String target, String data)
