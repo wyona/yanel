ff1eccf0d6da6612a78baa34ecf46e0c5fc7babd
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2011-01-05 06:36:03 +0000

    get current year dynamically

--- src/webapp/src/java/org/wyona/yanel/servlet/About.java
+++ src/webapp/src/java/org/wyona/yanel/servlet/About.java
@@ -11,8 +11,16 @@ public class About {
     public static String toHTML(String version, String revision) {
         StringBuilder sb = new StringBuilder("<html>");
         sb.append("<head><title>About Yanel</title></head>");
-        sb.append("<body><h1>About Yanel</h1><p>Version " + version + "-r" + revision + "</p><p>Copyright &#169; 2005 - 2010 Wyona. All rights reserved.</p></body>");
+        sb.append("<body><h1>About Yanel</h1><p>Version " + version + "-r" + revision + "</p><p>Copyright &#169; 2005 - " + getYear() + " Wyona. All rights reserved.</p></body>");
         sb.append("</html>");
         return sb.toString();
     }
+
+    /**
+     * Get current year
+     */
+     private static String getYear() {
+         java.text.DateFormat df = new java.text.SimpleDateFormat("yyyy");
+         return df.format(new java.util.Date());
+     }
 }
