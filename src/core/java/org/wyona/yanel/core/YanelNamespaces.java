29e58b58f5c54ed1778401c53f1d0c175d9578e0
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2011-03-10 08:54:45 +0000

    namespaces added

---
+++ src/core/java/org/wyona/yanel/core/YanelNamespaces.java
@@ -0,0 +1,14 @@
+package org.wyona.yanel.core;
+
+/**
+ * Various namespaces used more generally
+ */
+public class YanelNamespaces {
+    public static final String YANEL_NS = "http://www.wyona.org/yanel/1.0";
+    public static final String YANEL_NS_PREFIX = "y";
+
+    // TODO: Add this to the security package
+    //public static final String YANEL_SEC_NS = "http://www.wyona.org/security/1.0";
+
+    public static final String YANEL_SEARCH_NS = "http://www.wyona.org/yanel/search/1.0";
+}
