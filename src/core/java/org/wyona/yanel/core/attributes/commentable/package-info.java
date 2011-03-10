2c480d1e7764eb15603f5ae8e55e0336caa63dce
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2011-03-10 09:19:55 +0000

    jaxb configs added

---
+++ src/core/java/org/wyona/yanel/core/attributes/commentable/package-info.java
@@ -0,0 +1,6 @@
+@javax.xml.bind.annotation.XmlSchema(
+        namespace = org.wyona.yanel.core.YanelNamespaces.YANEL_NS, 
+        xmlns = { @javax.xml.bind.annotation.XmlNs(prefix = org.wyona.yanel.core.YanelNamespaces.YANEL_NS_PREFIX, 
+                namespaceURI = org.wyona.yanel.core.YanelNamespaces.YANEL_NS) }, 
+        elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.wyona.yanel.core.attributes.commentable;
