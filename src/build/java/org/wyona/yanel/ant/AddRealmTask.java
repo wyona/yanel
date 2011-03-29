a5cc8ebe7fa45d1b58d5fb2a19974ef61eabb731
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2011-03-29 13:21:27 +0000

    add attributes

--- src/build/java/org/wyona/yanel/ant/AddRealmTask.java
+++ src/build/java/org/wyona/yanel/ant/AddRealmTask.java
@@ -5,17 +5,28 @@ package org.wyona.yanel.ant;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.Path;
 
 /**
- *
+ * Add a third party realm to the local realm configuration
  */
 public class AddRealmTask extends Task {
+
+    private Path newRealmConfig;
 
     /**
      *
      */
     public void execute() throws BuildException {
         log("Not implemented yet!");
+        log("New (third party) realm configuration: " + newRealmConfig);
         //RealmManager rm = new RealmManager();
+    }
+
+    /**
+     * Ant file task attribute new (third party) realm config
+     */
+    public void setNewRealmConfig(Path newRealmConfig) {
+        this.newRealmConfig = newRealmConfig;
     }
 }
