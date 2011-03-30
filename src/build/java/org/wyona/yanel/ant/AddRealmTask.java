d92b89409546c90f167f5b2598890c16b0d3175d
ioannis <ioannis@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2011-03-30 16:57:27 +0000

    Implemented the add-realm task

--- src/build/java/org/wyona/yanel/ant/AddRealmTask.java
+++ src/build/java/org/wyona/yanel/ant/AddRealmTask.java
@@ -1,30 +1,81 @@
 /*
- *
+ * Adding a new (third party) realm to a Yanel instance
  */
 package org.wyona.yanel.ant;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.types.Path;
+import org.apache.commons.io.FileUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.wyona.commons.xml.XMLHelper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.w3c.dom.Document;
 
 /**
  * Add a third party realm to the local realm configuration
  */
 public class AddRealmTask extends Task {
 
-    private Path newRealmConfig;
+	private Path newRealmConfig;
     private String newRealmMountPoint;
     private String newRealmId;
-
-    /**
-     *
-     */
+    private Path defaultRealmsXml;
+    private Path localRealmsXml;
+    
     public void execute() throws BuildException {
-        log("Not implemented yet!");
-        log("New (third party) realm configuration: " + newRealmConfig);
+    	log("New (third party) realm configuration: " + newRealmConfig);
         log("New (third party) realm mount point: " + newRealmMountPoint);
         log("New (third party) realm id: " + newRealmId);
-        //RealmManager rm = new RealmManager();
+        
+        log("defaultRealmsXml: " + defaultRealmsXml);
+        log("localRealmsXml: " + localRealmsXml);
+        
+        if (!new File(localRealmsXml.toString()).exists()) {
+        	try {
+        		FileUtils.copyFile(new File(defaultRealmsXml.toString()), new File(localRealmsXml.toString()));
+        	} catch (IOException e) {
+        		log("Exception: " + e.getMessage());
+        	}
+        }
+        
+        try {
+        	Document doc = XMLHelper.readDocument(new FileInputStream(new File(localRealmsXml.toString())));
+        	Element rootElement = doc.getDocumentElement();
+        	// TODO check if realm with this id already exists
+              	
+        	Element realm = doc.createElementNS(rootElement.getNamespaceURI(), "realm");
+        	realm.setAttribute("id", newRealmId);
+        	realm.setAttribute("mount-point", newRealmMountPoint);
+        	realm.appendChild(doc.createTextNode("\n  ")); // formatting
+        	
+        	Element config = doc.createElementNS(rootElement.getNamespaceURI(), "config");
+        	String configSrcAttribute = newRealmConfig.toString();
+        	if (!configSrcAttribute.endsWith("/")) {
+        		configSrcAttribute += "/";
+        	}
+        	config.setAttribute("src", configSrcAttribute);
+        	      	
+        	realm.appendChild(config);
+        	realm.appendChild(doc.createTextNode("\n")); // formatting
+        	rootElement.appendChild(doc.createComment("\nAnt Generated Entry Using:\n./build.sh add-realm -Drealm-config=" + newRealmConfig + " -Drealm-mount-point=" + newRealmMountPoint + " -Drealm-id=" + newRealmId + "\n"));
+        	rootElement.appendChild(doc.createTextNode("\n")); // formatting
+        	rootElement.appendChild(realm);
+        	rootElement.appendChild(doc.createTextNode("\n\n")); // formatting
+
+        	log("Write new file: " + localRealmsXml);
+        	XMLHelper.writeDocument(doc, new FileOutputStream(localRealmsXml.toString()));
+        	        	
+        } catch (Exception e) {
+        	log("Exception: " + e.getMessage());
+        }
+
     }
 
     /**
@@ -47,8 +98,19 @@ public class AddRealmTask extends Task {
     public void setNewRealmId(String newRealmId) {
         this.newRealmId = newRealmId;
     }
+    
+    /**
+     * Ant file task attribute default realms.xml path
+     */
+    public void setDefaultRealmsXml(Path defaultRealmsXml) {
+        this.defaultRealmsXml = defaultRealmsXml;
+    }
+    
+    /**
+     * Ant file task attribute local realms.xml path
+     */
+    public void setLocalRealmsXml(Path localRealmsXml) {
+        this.localRealmsXml = localRealmsXml;
+    }
   
 }
-
-
-// TODO in the execude methods check whethere or not the directory exists
