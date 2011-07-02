1df7fabc32f18e4235a67d0a7fb329b0f297ee93
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2011-07-02 08:25:53 +0000

    catch possible null pointer

--- src/resources/navigation/src/java/org/wyona/yanel/impl/resources/navigation/NavigationResource.java
+++ src/resources/navigation/src/java/org/wyona/yanel/impl/resources/navigation/NavigationResource.java
@@ -391,18 +391,24 @@ public class NavigationResource extends Resource implements ViewableV2, Modifiab
 
     /**
      * Get client
+     * @param userAgent User agent
      */
     public String getClient(String userAgent) {
-        if (userAgent.indexOf("Firefox") > 0) {
-            return "firefox";
-        } else if (userAgent.indexOf("MSIE") > 0) {
-            return "msie";
-        } else if (userAgent.indexOf("Chrome") > 0) { // INFO: Please note that the chrome user agent also contains the word Safari, e.g. "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_7) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.77 Safari/534.24"
-            return "chrome";
-        } else if (userAgent.indexOf("Safari") > 0) {
-            return "safari";
+        if (userAgent != null) {
+            if (userAgent.indexOf("Firefox") > 0) {
+                return "firefox";
+            } else if (userAgent.indexOf("MSIE") > 0) {
+                return "msie";
+            } else if (userAgent.indexOf("Chrome") > 0) { // INFO: Please note that the chrome user agent also contains the word Safari, e.g. "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_7) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.77 Safari/534.24"
+                return "chrome";
+            } else if (userAgent.indexOf("Safari") > 0) {
+                return "safari";
+            } else {
+                log.warn("Client could not be recognized: " + userAgent);
+                return null;
+            }
         } else {
-            log.warn("Client could not be recognized: " + userAgent);
+            log.warn("No user agent specified");
             return null;
         }
     }
