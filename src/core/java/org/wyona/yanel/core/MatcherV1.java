d60c34db800ff6c1ff088835896d57ca720cede8
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2011-08-29 12:36:53 +0000

    matcher interface started

---
+++ src/core/java/org/wyona/yanel/core/MatcherV1.java
@@ -0,0 +1,12 @@
+package org.wyona.yanel.core;
+
+/**
+ *
+ */
+public interface MatcherV1 {
+
+    /**
+     *
+     */
+    public boolean match(org.wyona.yanel.core.map.Realm realm, String path, javax.servlet.http.HttpServletRequest request) throws Exception;
+}
