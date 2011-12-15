41e5f53b682cf55d0c41896a5faf33bbad789553
Michael Wechner <michael.wechner@wyona.com>
2011-12-15 11:11:34 +0100

    clean search index resource started

---
+++ src/contributions/resources/search/htdocs/clean-index.xsl
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<xsl:stylesheet version="1.0"
+  xmlns="http://www.w3.org/1999/xhtml"
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml"
+  xmlns:y="http://www.wyona.org/yanel/reindex/1.0">
+
+  <xsl:template match="/y:reindex">
+    <html>
+      <head>
+        <title>Clean index of repository</title>
+      </head>
+      <body>
+        <h1>Clean index of repository</h1>
+        <xsl:apply-templates select="y:exception"/>
+        <xsl:apply-templates select="y:message"/>
+
+<!--
+        <xsl:if test="not(y:exception or y:message)">
+-->
+          <p>Choose the repository for which you would like to clean the index (Please note that you will be asked to confirm before the actual cleaning will start):</p>
+          <ul>
+            <xsl:for-each select="y:repository">
+              <li>
+                <a href="?repository={@id}"><xsl:value-of select="."/></a></li>
+            </xsl:for-each>
+          </ul>
+<!--
+        </xsl:if>
+-->
+      </body>
+    </html>
+  </xsl:template>
+
+  <xsl:template match="y:exception">
+    <p style="color:red;"><xsl:value-of select="."/></p>
+  </xsl:template>
+
+  <xsl:template match="y:message">
+    <p style="color:green;"><xsl:value-of select="."/></p>
+  </xsl:template>
+
+</xsl:stylesheet>
---
+++ src/contributions/resources/search/src/java/org/wyona/yanel/impl/resources/search/CleanIndexResource.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2011 Wyona
+ */
+package org.wyona.yanel.impl.resources.search;
+
+import org.wyona.yarep.util.YarepUtil;
+import org.wyona.yarep.core.Repository;
+
+import org.wyona.yanel.core.attributes.viewable.View;
+import org.wyona.yanel.impl.resources.BasicXMLResource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This resource can be used to clean the index of a repository.
+ * If you configure this resource in your realm, make sure to protect it because you
+ * most likely don't want your users to start re-indexing processes.
+ */
+public class CleanIndexResource extends BasicXMLResource {
+    private static Logger log = Logger.getLogger(CleanIndexResource.class);
+
+    private static String REPO_NAME = "repository";
+    private static String REINDEX_XMLNS = "http://www.wyona.org/yanel/reindex/1.0";
+    
+    /**
+     * @see org.wyona.yanel.impl.resources.BasicXMLResource#getContentXML(String)
+     */
+    protected InputStream getContentXML(String viewId) throws Exception {
+        // Build output document
+        StringBuilder sb = new StringBuilder("<?xml version=\"1.0\"?>");
+        sb.append("<r:reindex xmlns:r=\"");
+        sb.append(REINDEX_XMLNS);
+        sb.append("\">");
+
+        // Which repo needs to be re-indexed?
+        String reindexRepo = getEnvironment().getRequest().getParameter(REPO_NAME); 
+
+        // Are we allowed to re-index this repository?
+        // Only default repositories and the ones listed in the resource configuration are allowed to be re-indexed
+        boolean allowed = false;
+
+        // List default repositories
+        // Only show them if we're allowed to re-index them
+        String defaultRepos = getResourceConfigProperty("allow-default-repos");
+        if("true".equals(defaultRepos)) {
+            sb.append("<r:repository id=\"yanel_data\">Realm's data repository</r:repository>");
+            sb.append("<r:repository id=\"yanel_ac-identities\">Realm's ac-identities repository</r:repository>");
+            sb.append("<r:repository id=\"yanel_ac-policies\">Realm's ac-policies repository</r:repository>");
+            sb.append("<r:repository id=\"yanel_res-configs\">Realm's res-configs repository</r:repository>");
+        }
+
+        // List extra repositories from repo configuration
+        String[] repoIds = getResourceConfigProperties("repository-id");
+        for(String repoId : repoIds) {
+            if(repoId != null && !"".equals(repoId)) {
+                sb.append("<r:repository id=\"");
+                sb.append(repoId);
+                sb.append("\">Configured repository with id '");
+                sb.append(repoId);
+                sb.append("'</r:repository>");
+                // Check  if repo that should be re-indexed is listed in resource configuration (see property 'repository-id')
+                if(!allowed && repoId.equals(reindexRepo)) allowed = true;
+            }
+        }
+
+        // Check if repo that should be re-indexed is default repo
+        if(!allowed && "true".equals(defaultRepos) &&
+          ("yanel_data".equals(reindexRepo) || 
+           "yanel_ac-policies".equals(reindexRepo) || 
+           "yanel_ac-identities".equals(reindexRepo) || 
+           "yanel_res-configs".equals(reindexRepo))) {
+                allowed = true;
+        }
+
+        // If it's an extra repo, allowed needs to be set to true
+        Repository repo = null;
+        if(allowed) {
+            try {
+                repo = getRealm().getRepository(reindexRepo);
+            } catch(Exception e) {
+                sb.append("<r:exception>Opening repo failed with exception: ");
+                sb.append(e.getMessage());
+                sb.append("</r:exception>");
+            }
+        }
+
+        // Perform re-indexing now
+        if(repo != null) {
+            YarepUtil yu = new YarepUtil();
+
+            try {
+                yu.indexRepository(repo);
+                sb.append("<r:message>Re-indexing was successful.</r:message>");
+            } catch(Exception e) {
+                sb.append("<r:exception>Re-indexing failed with exception: ");
+                sb.append(e.getMessage());
+                sb.append("</r:exception>");
+            }
+        } else if(reindexRepo != null) {
+            sb.append("<r:exception>Repository '" + reindexRepo + "' does either not exist or is not configured in order to be re-indexed.</r:exception>");
+        }
+
+        sb.append("</r:reindex>");
+        return new ByteArrayInputStream(sb.toString().getBytes());
+    }
+
+    /**
+     * @see org.wyona.yanel.core.api.attributes.ViewableV2#exists()
+     */
+    public boolean exists() throws Exception {
+        return true;
+    }
+}
