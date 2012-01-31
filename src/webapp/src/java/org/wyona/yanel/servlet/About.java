fd6753380bf62cb63e08d9d0ebf1768febddc9ad
Michael Wechner <michael.wechner@wyona.com>
2012-01-31 15:46:03 +0100

    catalina/tomcat home added and formatting

--- src/webapp/src/java/org/wyona/yanel/servlet/About.java
+++ src/webapp/src/java/org/wyona/yanel/servlet/About.java
@@ -12,9 +12,13 @@ public class About {
         StringBuilder sb = new StringBuilder("<html>");
         sb.append("<head><title>About Yanel</title></head>");
         sb.append("<body><h1>About Yanel</h1>");
-        sb.append("<p>Yanel Version: " + version + "-r" + revision + "</p>");
-        sb.append("<p>Java Version: " + System.getProperty("java.version") + "</p>");
-        sb.append("<p>Java Home: " + System.getProperty("java.home") + "</p>");
+        sb.append("<p>Yanel Version: <code>" + version + "-r" + revision + "</code></p>");
+        sb.append("<p>Java Version: <code>" + System.getProperty("java.version") + "</code></p>");
+        sb.append("<p>Java Home: <code>" + System.getProperty("java.home") + "</code></p>");
+        String catalinaHome = System.getProperty("catalina.home");
+        if (catalinaHome != null) {
+            sb.append("<p>Tomcat Home: <code>" + catalinaHome + "</code></p>");
+        }
         sb.append("<p>Copyright &#169; 2005 - " + getYear() + " <a href=\"http://www.wyona.com\">Wyona</a>. All rights reserved.</p>");
         sb.append("</body>");
         sb.append("</html>");
