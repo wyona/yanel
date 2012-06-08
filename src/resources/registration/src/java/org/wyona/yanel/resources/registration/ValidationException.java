835a63cbdf95f2b2a6ea39181a58af31bc15cbb2
Michael Wechner <michael.wechner@wyona.com>
2012-06-08 12:12:08 +0200

    single validation error added

--- src/resources/registration/src/java/org/wyona/yanel/resources/registration/ValidationException.java
+++ src/resources/registration/src/java/org/wyona/yanel/resources/registration/ValidationException.java
@@ -7,4 +7,25 @@ package org.wyona.yanel.resources.registration;
  *
  */
 public class ValidationException extends Exception {
+
+    private String key;
+    private String value;
+    private String errorCode;
+
+    /**
+     *
+     */
+    public ValidationException(String key, String value, String errorCode) {
+        this.key = key;
+        this.value = value;
+        this.errorCode = errorCode;
+    }
+
+    /**
+     * @see
+     */
+    @Override
+    public String getMessage() {
+        return key + ", " + value + ", " + errorCode;
+    }
 }
