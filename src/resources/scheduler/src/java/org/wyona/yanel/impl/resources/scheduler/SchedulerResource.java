dd9264128117c59b0febb6810943489056d184b1
Michael Wechner <michael.wechner@wyona.com>
2012-07-16 11:21:44 +0200

    scheduler resource started

---
+++ src/resources/scheduler/src/java/org/wyona/yanel/impl/resources/scheduler/SchedulerResource.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2010 Wyona
+ */
+
+package org.wyona.yanel.impl.resources.scheduler;
+
+import org.wyona.yanel.impl.resources.BasicXMLResource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.apache.log4j.Logger;
+
+/**
+ * A simple Resource which extends BasicXMLResource
+ */
+public class SchedulerResource extends BasicXMLResource {
+    
+    private static Logger log = Logger.getLogger(SchedulerResource.class);
+    
+    /**
+     * This method overrides the method to create the InputStream called by BasicXMLResource
+     * Since you extend the BasicXMLResource this has to contain well-formed xml.
+     * Should return a InputStream which contains XML. 
+     * Use String, StingBuffer, dom, jdom, org.apache.commons.io.IOUtils and so on to generate the XML.
+     */
+    protected InputStream getContentXML(String viewId) throws Exception {
+        if (log.isDebugEnabled()) {
+            log.debug("requested viewId: " + viewId);
+        }
+        StringBuilder sb = new StringBuilder("<?xml version=\"1.0\"?>");
+        sb.append("<root>");
+ 
+        sb.append("</root>");
+        return new ByteArrayInputStream(sb.toString().getBytes());
+    }
+}
