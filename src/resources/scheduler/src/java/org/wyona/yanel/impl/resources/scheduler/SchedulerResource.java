26bb71415c47d0ab6abf3fb8ce292f7617ec0565
Michael Wechner <michael.wechner@wyona.com>
2012-07-17 01:10:43 +0200

    list jobs

--- src/resources/scheduler/src/java/org/wyona/yanel/impl/resources/scheduler/SchedulerResource.java
+++ src/resources/scheduler/src/java/org/wyona/yanel/impl/resources/scheduler/SchedulerResource.java
@@ -11,6 +11,11 @@ import java.io.InputStream;
 
 import org.apache.log4j.Logger;
 
+import org.quartz.JobDetail;
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.impl.matchers.GroupMatcher;
+
 /**
  * Scheduler jobs overview
  */
@@ -26,9 +31,25 @@ public class SchedulerResource extends BasicXMLResource {
         if (log.isDebugEnabled()) {
             log.debug("requested viewId: " + viewId);
         }
+
+        java.util.Collection<Scheduler> schedulers = new org.quartz.impl.StdSchedulerFactory().getAllSchedulers();
+
         StringBuilder sb = new StringBuilder("<?xml version=\"1.0\"?>");
         sb.append("<scheduler>");
- 
+        if (schedulers == null || schedulers.isEmpty()) {
+            sb.append("<no-schedulers-running/>");
+        } else {
+            sb.append("<number-of-schedulers>" + schedulers.size() + "</number-of-schedulers>");
+            java.util.Iterator schedulerIt  = schedulers.iterator();
+            Scheduler scheduler = (Scheduler) schedulerIt.next();
+            String name = getRealm().getID();
+            java.util.Set keys = scheduler.getJobKeys(GroupMatcher.jobGroupEquals(name));
+            java.util.Iterator keysIt = keys.iterator();
+            while(keysIt.hasNext()) {
+                JobDetail jd = scheduler.getJobDetail((JobKey)keysIt.next());
+                sb.append("<job>" + jd.getDescription() + "</job>");
+            }
+        }
         sb.append("</scheduler>");
         return new ByteArrayInputStream(sb.toString().getBytes());
     }
