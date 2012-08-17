859415d2af93ebc213bd7df4c6305e2f9ac374ae
Cedric Staub <cs.staub@gmail.com>
2012-08-17 14:05:21 +0200

    Work on personalized-content resource

--- src/resources/personalized-content/src/java/org/wyona/yanel/impl/resources/boost/PersonalizedContentResource.java
+++ src/resources/personalized-content/src/java/org/wyona/yanel/impl/resources/boost/PersonalizedContentResource.java
@@ -4,131 +4,96 @@
 
 package org.wyona.yanel.impl.resources.boost;
 
-import org.wyona.yanel.impl.resources.BasicXMLResource;
-
-import org.wyona.commons.xml.XMLHelper;
-
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.Date;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import org.wyona.yanel.servlet.AccessLog;
+import org.wyona.yanel.impl.resources.BasicXMLResource;
+import org.wyona.commons.xml.XMLHelper;
 
 import org.apache.log4j.Logger;
 
+import org.wyona.boost.client.BoostService;
+import org.wyona.boost.client.ServiceException;
+import org.wyona.boost.client.BoostServiceConfig;
+
 /**
- * Generate personalized content based on user's interests
+ * Retrieve the user profile of the current user, given his cookie id, from
+ * the Boost service using the Boost client library.
  */
 public class PersonalizedContentResource extends BasicXMLResource {
-    
+
+    @SuppressWarnings("unused")
     private static Logger log = Logger.getLogger(PersonalizedContentResource.class);
-    
+
+    private static final String NAMESPACE = "http://www.wyona.org/yanel/boost/1.0";
+
     /**
+     * Get the XML content of this resource.
      * @see org.wyona.yanel.impl.resources.BasicXMLResource#getContentXML(String)
      */
     protected InputStream getContentXML(String viewId) throws Exception {
-        if (log.isDebugEnabled()) {
-            log.debug("requested viewId: " + viewId);
-        }
+        Document doc = XMLHelper.createDocument(NAMESPACE, "personalized-content");
+        Element root = doc.getDocumentElement();
 
-        final String NAMESPACE = "http://www.wyona.org/yanel/boost/1.0";
-        org.w3c.dom.Document doc = XMLHelper.createDocument(NAMESPACE, "personalized-content");
-        org.w3c.dom.Element rootEl = doc.getDocumentElement();
-
+        // Is the user logged into Yanel?
         String username = getEnvironment().getIdentity().getUsername();
-        if (username != null) {
-            rootEl.setAttributeNS(NAMESPACE, "user-id", username);
-        } else {
-            rootEl.appendChild(doc.createElementNS(NAMESPACE, "no-username-yet"));
+        if(username != null) {
+            Element uid = doc.createElement("yanel-user-id");
+            uid.appendChild(doc.createTextNode(username));
         }
 
-        //javax.servlet.http.Cookie cookie = org.wyona.yanel.servlet.AccessLog.getYanelAnalyticsCookie(getEnvironment().getRequest(), getEnvironment().getResponse());
-        javax.servlet.http.Cookie cookie = org.wyona.yanel.servlet.AccessLog.getYanelAnalyticsCookie(getEnvironment().getRequest());
-        if (cookie != null) {
-            Date lastAccess = getLastAccess(cookie.getValue(), "www.yanel.org");
-            java.text.DateFormat df = new java.text.SimpleDateFormat("yyyyMMdd");
+        // Get the cookie
+        HttpServletRequest req = getEnvironment().getRequest();
+        Cookie cookie = AccessLog.getYanelAnalyticsCookie(req);
 
-            rootEl.setAttributeNS(NAMESPACE, "cookie-id", cookie.getValue());
-            rootEl.setAttributeNS(NAMESPACE, "last-access", df.format(lastAccess));
+        if(cookie == null) {
+            root.appendChild(doc.createElementNS(NAMESPACE, "no-cookie-yet"));
+            return XMLHelper.getInputStream(doc, false, false, null);
+        }
 
-            org.w3c.dom.Element sinceLastAccessEl = doc.createElementNS(NAMESPACE, "since-last-access");
-            rootEl.appendChild(sinceLastAccessEl);
+        Element cookieEl = doc.createElementNS(NAMESPACE, "cookie-id");
+        cookieEl.appendChild(doc.createTextNode(cookie.getValue()));
+        root.appendChild(cookieEl);
 
-            org.w3c.dom.Element beforeLastAccessEl = doc.createElementNS(NAMESPACE, "before-last-access");
-            rootEl.appendChild(beforeLastAccessEl);
+        try {
+            Iterable<String> userInterests = getUserInterests(cookie.getValue());
 
-            String[] userInterests = getUserInterests(cookie.getValue());
-            if (userInterests != null && userInterests.length > 0) {
-                for (int k = 0; k < userInterests.length; k++) {
-                    //String queryInclModDate = userInterests[k] + " AND mod_date:[" + df.format(lastAccess) + " TO " + df.format(new Date()) + "]"; // INFO: See http://lucene.apache.org/java/2_3_2/queryparsersyntax.html#Range%20Searches
-                    String queryInclModDate = userInterests[k] + " AND yarep_lastModified:[" + lastAccess.getTime() + " TO " + new Date().getTime() + "]"; // INFO: See http://lucene.apache.org/java/2_3_2/queryparsersyntax.html#Range%20Searches
-                    log.warn("DEBUG: Query: " + queryInclModDate);
-
-                    org.wyona.yarep.core.Node[] nodesInclModDate = getRealm().getRepository().getSearcher().search(queryInclModDate);
-
-                    if (nodesInclModDate != null && nodesInclModDate.length > 0) {
-                        for (int i = 0; i < nodesInclModDate.length; i++) {
-                            org.w3c.dom.Element result = doc.createElementNS(NAMESPACE, "result");
-                            result.setAttributeNS(NAMESPACE, "node-path", nodesInclModDate[i].getPath());
-                            result.setAttributeNS(NAMESPACE, "node-last-modified", "" + new Date(nodesInclModDate[i].getLastModified()));
-                            result.setAttributeNS(NAMESPACE, "interest", userInterests[k]);
-                            sinceLastAccessEl.appendChild(result);
-                        }
-                    } else {
-                        org.w3c.dom.Element noResults = doc.createElementNS(NAMESPACE, "no-results");
-                        noResults.setAttributeNS(NAMESPACE, "interest", userInterests[k]);
-                        sinceLastAccessEl.appendChild(noResults);
-                    }
-
-                    String query = userInterests[k];
-                    org.wyona.yarep.core.Node[] nodes = getRealm().getRepository().getSearcher().search(query);
-                    if (nodes != null && nodes.length > 0) {
-                        for (int i = 0; i < nodes.length; i++) {
-                            if (nodes[i].getLastModified() < lastAccess.getTime()) {
-                                org.w3c.dom.Element result = doc.createElementNS(NAMESPACE, "result");
-                                result.setAttributeNS(NAMESPACE, "node-path", nodes[i].getPath());
-                                result.setAttributeNS(NAMESPACE, "node-last-modified", "" + new Date(nodes[i].getLastModified()));
-                                result.setAttributeNS(NAMESPACE, "interest", userInterests[k]);
-                                beforeLastAccessEl.appendChild(result);
-                            }
-                        }
-                    } else {
-                        org.w3c.dom.Element noResults = doc.createElementNS(NAMESPACE, "no-results");
-                        noResults.setAttributeNS(NAMESPACE, "interest", userInterests[k]);
-                        beforeLastAccessEl.appendChild(noResults);
-                    }
-                }
-            } else {
-                rootEl.appendChild(doc.createElementNS(NAMESPACE, "no-user-interests"));
+            for(String interest : userInterests) {
+                Element interestEl = doc.createElementNS(NAMESPACE, "interest");
+                interestEl.appendChild(doc.createTextNode(interest));
+                root.appendChild(interestEl);
             }
-        } else {
-            rootEl.appendChild(doc.createElementNS(NAMESPACE, "no-cookie-yet"));
+        } catch(ServiceException e) {
+            // No interests
+            Element errEl = doc.createElementNS(NAMESPACE, "service-not-available");
+            root.appendChild(errEl);
         }
 
         return XMLHelper.getInputStream(doc, false, false, null);
     }
 
     /**
-     * Get user's interests (TODO: Use org.wyona.boost.client...)
-     * @param Unique cookie ID
+     * Get the user profile given a cookie.
+     * @param cookie Unique cookie id
      */
-    protected String[] getUserInterests(String cookieID) {
-        String[] interests = new String[3];
-        interests[0] = "Lucene";
-        interests[1] = "OSGi";
-        interests[2] = "Switzerland";
-        return interests;
+    protected Iterable<String> getUserInterests(String cookie)
+    throws Exception {
+
+        BoostServiceConfig bsc = new BoostServiceConfig(
+            "http://localhost:9080/boost/reasoner", "globus");
+
+        BoostService boost = new BoostService(bsc);
+        return boost.getUserProfile(cookie);
     }
 
     /**
-     * Get user's last access (TODO: Use org.wyona.boost.client...)
-     * @param Unique cookie ID
-     */
-    protected Date getLastAccess(String cookieID, String domain) throws Exception {
-        java.text.DateFormat df = new java.text.SimpleDateFormat("yyyy.MM.dd");
-        return df.parse("2011.02.16");
-    }
-
-    /**
-     * @see org/wyona/yanel/core/api/attributes/ViewableV2#exists()
+     * Do we exist? Returns 404 if we answer no.
+     * @see org.wyona.yanel.core.api.attributes.ViewableV2#exists()
      */
     public boolean exists() throws Exception {
         return true;
