8dbe5c358a42c3a134a29bd54d864cab93310a68
Michael Wechner <michael.wechner@wyona.com>
2013-01-25 21:14:25 +0100

    javadoc and message improved

--- src/resources/registration/src/java/org/wyona/yanel/resources/registration/ValidationException.java
+++ src/resources/registration/src/java/org/wyona/yanel/resources/registration/ValidationException.java
@@ -4,7 +4,7 @@
 package org.wyona.yanel.resources.registration;
 
 /**
- *
+ * Exception containing validation errors
  */
 public class ValidationException extends Exception {
 
@@ -35,13 +35,13 @@ public class ValidationException extends Exception {
     }
 
     /**
-     * @see
+     * @see java.lang.Throwable#getMessage()
      */
     @Override
     public String getMessage() {
         if (ves != null && ves.size() > 0) {
             ValidationError ve = (ValidationError) ves.get(0);
-            return ve.getKey() + ", " + ve.getValue() + ", " + ve.getErrorCode();
+            return "'" + ve.getKey() + "', '" + ve.getValue() + "', '" + ve.getErrorCode() + "'";
         } else {
             return "No validation errors!";
         }
