05523148ab2e9a63fddebe4f59d22ad37384a0d1
Michael Wechner <michael.wechner@wyona.com>
2013-06-22 17:14:09 +0200

    some first logic implemented

--- src/webapp/src/java/org/wyona/yanel/servlet/security/impl/CASWebAuthenticatorImpl.java
+++ src/webapp/src/java/org/wyona/yanel/servlet/security/impl/CASWebAuthenticatorImpl.java
@@ -38,11 +38,16 @@ public class CASWebAuthenticatorImpl implements WebAuthenticator {
 
     private static Logger log = Logger.getLogger(CASWebAuthenticatorImpl.class);
 
+    private String loginURL;
+    private String validateURL;
+
     /**
      * @see org.wyona.yanel.core.api.security.WebAuthenticator#init(Document, URIResolver)
      */
     public void init(Document configuration, URIResolver resolver) throws Exception {
         log.warn("TODO: Finish implementation!");
+        loginURL = "https://127.0.0.1:9090/cas-server-webapp-3.5.2/login";
+        validateURL = "https://127.0.0.1:9090/cas-server-webapp-3.5.2/serviceValidate";
     }
 
     /**
@@ -57,6 +62,28 @@ public class CASWebAuthenticatorImpl implements WebAuthenticator {
      */
     public HttpServletResponse doAuthenticate(HttpServletRequest request, HttpServletResponse response, Map map, String reservedPrefix, String xsltLoginScreenDefault, String servletContextRealPath, String sslPort) throws ServletException, IOException {
         log.warn("TODO: Finish implementation!");
+        String casTicket = request.getParameter("ticket");
+        if (casTicket != null) {
+            String username = validate(casTicket);
+            if (username != null) {
+                // TODO: Add username to session and hence consider user authenticated
+                return null; // TODO: Redirect to original request
+            } else {
+                log.warn("Validation of ticket '" + casTicket + "' failed!");
+                return null; // TODO: Redirect user to CAS login page
+            }
+        } else {
+            return null; // TODO: Redirect user to CAS login page
+        }
+    }
+
+    /**
+     * Validate CAS ticket
+     * @param ticket CAS ticket
+     * @return username associated with ticket when ticket is valid, return null otherwise
+     */
+    private String validate(String ticket) {
+        log.warn("TODO: Validate ticket '" + ticket + "' at '" + validateURL + "'...");
         return null;
     }
 }
