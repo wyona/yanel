ea74a9486e6475cab31060d3546f91ae62c0eceb
Michael Wechner <michael.wechner@wyona.com>
2013-12-12 18:05:00 +0100

    get max file size from configuration

--- src/webapp/src/java/org/wyona/yanel/servlet/communication/YanelFilter.java
+++ src/webapp/src/java/org/wyona/yanel/servlet/communication/YanelFilter.java
@@ -13,10 +13,10 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package org.wyona.yanel.servlet.communication;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -25,15 +25,36 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+/**
+ * Intercept requests in order to handle multipart requests
+ */
 public class YanelFilter implements Filter {
+
+    private static final Logger log = LogManager.getLogger(YanelFilter.class);
+
+    private long maxFileSize;
+
+    /**
+     * @see javax.servlet.Filter#init(FilterConfig)
+     */
     public void init(FilterConfig filterConfig) throws ServletException {
+        maxFileSize = new Long(filterConfig.getInitParameter("max-file-size")).longValue();
+        log.warn("DEBUG: Max file size: " + maxFileSize);
     }
 
+    /**
+     * @see javax.servlet.Filter#destroy()
+     */
     public void destroy() {
     }
 
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        chain.doFilter(new HttpRequest((HttpServletRequest) request), response);
+    /**
+     * @see javax.servlet.Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        chain.doFilter(new HttpRequest((HttpServletRequest) request, maxFileSize), response);
     }
 }
