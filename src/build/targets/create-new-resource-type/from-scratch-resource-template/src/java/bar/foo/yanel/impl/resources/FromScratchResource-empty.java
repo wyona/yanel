dd13031c4d4d6efb05e05f44db77349047157841
Michael Wechner <michael.wechner@wyona.com>
2014-03-27 14:39:23 +0100

    copyright and logger updated

--- src/build/targets/create-new-resource-type/from-scratch-resource-template/src/java/bar/foo/yanel/impl/resources/FromScratchResource-empty.java
+++ src/build/targets/create-new-resource-type/from-scratch-resource-template/src/java/bar/foo/yanel/impl/resources/FromScratchResource-empty.java
@@ -1,19 +1,20 @@
 /*
- * Copyright 2007 Wyona
+ * Copyright 2014 Wyona
  */
 
 package bar.foo.yanel.impl.resources;
 
 import org.wyona.yanel.core.Resource;
 @IMPORT_REPLACE_TOKEN@
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 /**
  *
  */
 public class FromScratchResource extends Resource @IMPLEMENTS_REPLACE_TOKEN@ @INTERFACE_REPLACE_TOKEN@ {
     
-    private static Logger log = Logger.getLogger(FromScratchResource.class);
+    private static Logger log = LogManager.getLogger(FromScratchResource.class);
 
 @METHODESTUB_REPLACE_TOKEN@
 }
