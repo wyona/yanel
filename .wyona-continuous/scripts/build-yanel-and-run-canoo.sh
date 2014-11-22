22530836fad32f09c8000d8ebc2cd2b71f12a743
Michael Wechner <michael.wechner@wyona.com>
2014-11-22 17:17:42 +0100

    scripts added

---
+++ .wyona-continuous/scripts/build-yanel-and-run-canoo.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Also see src/build/targets/continuous-integration/reCI
+
+YANEL_SRC=~/projects/yanel/repositories/yanel
+
+echo "Build Yanel and run Canoo Web Tests."
+
+cd $YANEL_SRC
+./yanel.sh stop
+./build.sh clean -DanswerCleanTomcatClusterNode1=yes
+./build.sh -Dgwt.compile.skip=true
+./yanel.sh start
+
+cd $YANEL_SRC/src/realms/yanel-website/src/test/canoo
+$YANEL_SRC/tools/apache-ant/bin/ant -Dwt.headless=
+
+cd $YANEL_SRC
+./yanel.sh stop
