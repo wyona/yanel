d8b9fd2f24aac32f6de4180805fbff755ce6e0f8
Michael Wechner <michael.wechner@wyona.com>
2015-02-22 23:22:52 +0100

    use shell script arguments

--- .wyona-continuous/scripts/build-resource-and-run-junit-tests.sh
+++ .wyona-continuous/scripts/build-resource-and-run-junit-tests.sh
@@ -11,11 +11,14 @@ echo "Parent directory: $PARENT_DIR"
 YANEL_SRC="$(dirname "$PARENT_DIR")"
 echo "Yanel source directory: $YANEL_SRC"
 
-CLASS_NAME=org.wyona.yanel.impl.resources.XMLResource
-#CLASS_NAME=$1
+#RESOURCE_DIR=xml
+RESOURCE_DIR=$1
 
-echo "Build resource and run junit tests of class $CLASS_NAME."
+#CLASS_NAME=org.wyona.yanel.impl.resources.XMLResource
+CLASS_NAME=$2
+
+echo "Build resource '$RESOURCE_DIR' and run junit tests of class '$CLASS_NAME'."
 
 cd $YANEL_SRC
-./build.sh -f src/resources/xml/build.xml
+./build.sh -f src/resources/${RESOURCE_DIR}/build.xml
 ./build.sh -f src/test/build.xml junit -Dtest.class.name=${CLASS_NAME}Test
