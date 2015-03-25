82c54ed95a2b356d5ed84f88624bd88014e16b24
Michael Wechner <michael.wechner@wyona.com>
2015-03-25 10:26:39 +0100

    yanel.revision added

--- .wyona-continuous/scripts/create-release.sh
+++ .wyona-continuous/scripts/create-release.sh
@@ -9,8 +9,13 @@ echo "Parent directory: $PARENT_DIR"
 YANEL_SRC="$(dirname "$PARENT_DIR")"
 echo "Yanel source directory: $YANEL_SRC"
 
+YANEL_REVISION=c76b2afc69489824bc8f9c1f05ffdcffcfa07c04
+
 cd $YANEL_SRC
-./build.sh source-snapshot
+
+./build.sh source-snapshot -Dyanel.revision=$YANEL_REVISION
 #./build.sh bin-snapshot
 
-#TODO: Secure copy releases to Yanel website http://www.yanel.org/en/download/unix.html
+scp build/source-snapshots/wyona-yanel-3.0.0-r${YANEL_REVISION}-src.zip wyona@www.yanel.org:src/yanel-node1/src/realms/yanel-website/data-repo/data/download/source-snapshots/.
+
+#TODO: Update Yanel website http://www.yanel.org/en/download/unix.html
