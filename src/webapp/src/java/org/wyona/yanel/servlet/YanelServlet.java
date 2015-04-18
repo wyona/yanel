cc636d216ebed61b7b67b99f413eeb20e9823a99
Michael Wechner <michael.wechner@wyona.com>
2015-04-18 18:37:19 +0200

    use Utils

--- src/webapp/src/java/org/wyona/yanel/servlet/YanelServlet.java
+++ src/webapp/src/java/org/wyona/yanel/servlet/YanelServlet.java
@@ -1365,66 +1365,8 @@ public class YanelServlet extends HttpServlet {
      */
     private String getRequestURLQS(HttpServletRequest request, String addQS, boolean xml) {
         try {
-            Realm realm = map.getRealm(request.getServletPath());
-    
-            // TODO: Handle this exception more gracefully!
-            if (realm == null) log.error("No realm found for path " +request.getServletPath());
-
-            String proxyHostName = realm.getProxyHostName();
-            int proxyPort = realm.getProxyPort();
-            String proxyPrefix = realm.getProxyPrefix();
-            ReverseProxyConfig reverseProxyConfig = realm.getReverseProxyConfig();
-    
-            URL url = new URL(request.getRequestURL().toString());
-
-            if(realm.isProxySet()) {
-                String forwardedHost = request.getHeader("X-FORWARDED-HOST");
-                if (forwardedHost != null) {
-                    url = new java.net.URL(url.getProtocol(), forwardedHost, url.getFile());
-                }
-                if (proxyHostName != null) {
-                    url = new URL(url.getProtocol(), proxyHostName, url.getFile());
-                }
-
-                if (proxyPort >= 0) {
-                    //log.debug("Configured proxy port: " + proxyPort);
-                    url = new URL(url.getProtocol(), url.getHost(), proxyPort, url.getFile());
-                } else {
-                    //log.debug("No proxy port configured, hence use default port: " + url.getDefaultPort());
-                    url = new URL(url.getProtocol(), url.getHost(), url.getFile()); // INFO: Please note that if one does not set the port explicitely, then toString() won't add the port to the returned string.
-                }
-
-                if (proxyPrefix != null) {
-                    // INFO: Remove proxy prefix, e.g. "/yanel/yanel-website"
-                    url = new URL(url.getProtocol(), url.getHost(), url.getPort(), url.getFile().substring(proxyPrefix.length()));
-                }
-
-                if (reverseProxyConfig.getReversePrefix() != null) {
-                    // INFO: Add reverse proxy prefix, e.g. "/boost2"
-                    url = new URL(url.getProtocol(), url.getHost(), url.getPort(), reverseProxyConfig.getReversePrefix() + url.getFile());
-                }
-
-                //log.debug("Proxy enabled for this realm resp. request: " + realm + ", " + url);
-            } else {
-                //log.debug("No proxy set for this realm resp. request: " + realm + ", " + url);
-            }
-
-            String urlQS = url.toString();
-            if (request.getQueryString() != null) {
-                urlQS = urlQS + "?" + request.getQueryString();
-                if (addQS != null) urlQS = urlQS + "&" + addQS;
-            } else {
-                if (addQS != null) urlQS = urlQS + "?" + addQS;
-            }
-    
-            if (xml) {
-                urlQS = urlQS.replaceAll("&", "&amp;");
-            }
-    
-            if(log.isDebugEnabled()) log.debug("Request: " + urlQS);
-
-            return urlQS;
-        } catch (Exception e) {
+            return Utils.getRequestURLQS(map.getRealm(request.getServletPath()), request, addQS, xml);
+        } catch(Exception e) {
             log.error(e.getMessage(), e);
             return null;
         }
