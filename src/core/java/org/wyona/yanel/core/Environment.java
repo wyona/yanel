cc0014694dcbe815d2e72bd4b89c455b03bee173
Michael Wechner <michael.wechner@wyona.com>
2015-06-10 11:26:08 +0200

    logging updated

--- src/core/java/org/wyona/yanel/core/Environment.java
+++ src/core/java/org/wyona/yanel/core/Environment.java
@@ -21,14 +21,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.wyona.security.core.api.Identity;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 /**
  * The environment stores items which belong to the environment from which a resource
  * is called, i.e. the request, the response, the identity, the state of view, and the resource container path.
  */
 public class Environment {
-    private static Logger log = Logger.getLogger(Environment.class);
+    private static Logger log = LogManager.getLogger(Environment.class);
 
     private HttpServletRequest request;
     private HttpServletResponse response;
