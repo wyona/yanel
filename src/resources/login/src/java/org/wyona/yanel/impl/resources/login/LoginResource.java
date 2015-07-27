db8289c4375e3d9b01abf8e18a78e97330d13c39
Michael Wechner <michael.wechner@wyona.com>
2015-07-27 16:17:49 +0200

    realm name and mount point added

--- src/resources/login/src/java/org/wyona/yanel/impl/resources/login/LoginResource.java
+++ src/resources/login/src/java/org/wyona/yanel/impl/resources/login/LoginResource.java
@@ -4,6 +4,7 @@
 package org.wyona.yanel.impl.resources.login;
 
 import org.wyona.yanel.impl.resources.BasicXMLResource;
+import org.wyona.yanel.servlet.YanelServlet;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -12,6 +13,9 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 import org.wyona.commons.xml.XMLHelper;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Document;
 
 /**
  * Login resource
@@ -29,7 +33,12 @@ public class LoginResource extends BasicXMLResource {
             log.debug("requested viewId: " + viewId);
         }
 
-        org.w3c.dom.Document doc = XMLHelper.createDocument(org.wyona.yanel.servlet.YanelServlet.NAMESPACE, "yanel-auth-screen");
+        Document doc = XMLHelper.createDocument(YanelServlet.NAMESPACE, "yanel-auth-screen");
+        Element rootElement = doc.getDocumentElement();
+
+        Element realmElement = (Element) rootElement.appendChild(doc.createElementNS(YanelServlet.NAMESPACE, "realm"));
+        realmElement.setAttributeNS(YanelServlet.NAMESPACE, "name", realm.getName());
+        realmElement.setAttributeNS(YanelServlet.NAMESPACE, "mount-point", realm.getMountPoint().toString());
 
         return XMLHelper.getInputStream(doc, false, true, null);
     }
