8933353784f339e724a6f70ebb9fe1f091a184d0
Michael Wechner <michael.wechner@wyona.com>
2015-12-28 12:43:47 +0100

    check for pre-authenticated user

--- src/webapp/src/java/org/wyona/yanel/servlet/YanelServlet.java
+++ src/webapp/src/java/org/wyona/yanel/servlet/YanelServlet.java
@@ -1921,6 +1921,25 @@ public class YanelServlet extends HttpServlet {
             return identity;
         }
 
+        String preAuthReqHeaderName = "COLOS_REMOTE_USER"; // TODO: For example from login resource: getResourceConfigProperty("pre-auth-request-header");
+        if (preAuthReqHeaderName != null && request.getHeader(preAuthReqHeaderName) != null) {
+            String preAuthUserName = request.getHeader(preAuthReqHeaderName);
+            log.warn("DEBUG: Pre authenticated user: " + preAuthUserName);
+            String trueID = realm.getIdentityManager().getUserManager().getTrueId(preAuthUserName);
+            User user = realm.getIdentityManager().getUserManager().getUser(trueID);
+            if (user != null) {
+                log.debug("User '" + user.getID() + "' considered pre-authenticated.");
+                identity = new Identity(user, preAuthUserName);
+                setIdentity(identity, request.getSession(true), realm);
+                return identity;
+            } else {
+                log.warn("No such pre-authenticated user '" + preAuthUserName + "', hence set identity to WORLD!");
+                identity = new Identity();
+                setIdentity(identity, request.getSession(true), realm);
+                return identity;
+            }
+        }
+
 /* TODO: Make configurable!
         if (true) {
             log.warn("DEBUG: Do not check for BASIC authentication, but set identity to WORLD.");
